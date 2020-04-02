e551a974a787d06cba38f889ccb20127b1474d4a
spookydonut <github@spooksoftware.com>
2020-04-02 11:03:25 +0800

    no docker

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#Project dependencies file
-#Final authority on what's required to fully build the project
-
 # byond version
-# Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
 # TODO: sed this from buildByond.conf
 export BYOND_MAJOR=513
 export BYOND_MINOR=1508
