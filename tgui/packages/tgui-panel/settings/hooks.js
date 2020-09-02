5789aa0e49b5104516c688958c1a51382c026f7c
ThePotato97 <christopher.mash.cm@gmail.com>
2020-09-02 01:53:14 +0100

    Fixed it maybe?

--- tgui/packages/tgui-panel/settings/hooks.js
+++ tgui/packages/tgui-panel/settings/hooks.js
@@ -1,5 +1,11 @@
-import { useDispatch, useSelector } from 'tgui/store';
-import { updateSettings } from './actions';
+/**
+ * @file
+ * @copyright 2020 Aleksej Komarov
+ * @license MIT
+ */
+
+import { useDispatch, useSelector } from 'common/redux';
+import { updateSettings, toggleSettings } from './actions';
 import { selectSettings } from './selectors';
 
 export const useSettings = context => {
@@ -7,6 +13,8 @@ export const useSettings = context => {
   const dispatch = useDispatch(context);
   return {
     ...settings,
+    visible: settings.view.visible,
+    toggle: () => dispatch(toggleSettings()),
     update: obj => dispatch(updateSettings(obj)),
   };
 };
