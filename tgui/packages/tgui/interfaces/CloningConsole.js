a08af5c784eeb070ab502a06a31760b535f20a1d
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2020-10-06 15:59:04 +0100

    UI changes to make things clearer

--- tgui/packages/tgui/interfaces/CloningConsole.js
+++ tgui/packages/tgui/interfaces/CloningConsole.js
@@ -77,8 +77,8 @@ export const CloningConsole = (props, context) => {
   return (
     <Window
       theme={cloneSlave ? "syndicate" : "ntos"}
-      width={550}
-      height={530}>
+      width={540}
+      height={700}>
       <Window.Content>
         {(deletionTarget && (
           <Modal
@@ -170,6 +170,7 @@ const Functions = (props, context) => {
     disk,
     allowMindErasure,
     mindWipe,
+    diskReadOnly,
   } = data;
 
   return (
@@ -222,19 +223,27 @@ const Functions = (props, context) => {
       ))}
       {(!!disk && (
         <Section
-          title="Disk Controls">
-          <Button
-            icon="upload"
-            color={"blue"}
-            onClick={() => act("load")}>
-            Load from disk
-          </Button>
-          <Button
-            icon="eject"
-            color={"red"}
-            onClick={() => act("eject")}>
-            Eject Disk
-          </Button>
+          title="Disk Controls"
+          buttons={
+            <Fragment>
+              <Button
+                icon="upload"
+                color={"blue"}
+                onClick={() => act("load")}>
+                Load from disk
+              </Button>
+              <Button
+                icon="eject"
+                color={"bad"}
+                onClick={() => act("eject")}>
+                Eject Disk
+              </Button>
+            </Fragment>
+          }>
+          <Box>
+            <Icon color={diskReadOnly ? "bad" : "good"} name={diskReadOnly ? "times" : "check"} />
+            {diskReadOnly ? " Disk is read only." : " Disk is writeable."}
+          </Box>
         </Section>
       ))}
     </Fragment>
@@ -256,43 +265,86 @@ const StatusSection = (props, context) => {
   const message = data.message || { text: "", status: "" };
 
   return (
-    <Section>
-      <LabeledList>
-        <LabeledList.Item label="Completion">
-          {!podGone && (
-            <ProgressBar
-              value={completion}
-              maxValue={100}
-              minValue={0}
-              ranges={{
-                good: [90, Infinity],
-                average: [25, 90],
-                bad: [-Infinity, 25],
-              }} />
-          )}
-          {!!podGone && (
-            "No Pod Detected"
-          )}
-        </LabeledList.Item>
-        <LabeledList.Item label="Bio-Matter">
-          {!podGone && (
-            <ProgressBar
-              value={meatLevels}
-              maxValue={100}
-              minValue={0}
-              ranges={{
-                good: [50, 100],
-                average: [25, 50],
-                bad: [0, 25],
-              }} />
-          )}
-          {!!podGone && (
-            "No Pod Detected"
-          )}
-        </LabeledList.Item>
-      </LabeledList>
-      <Flex>
-        <Flex.Item mt={3.5} width={scannerGone ? 16 : 15}>
+    <Fragment>
+      <Section
+        title="Status Messages"
+        height={7}>
+        {message.text && (
+          <TypedNoticeBox
+            type={message.status}
+            textColor="white"
+            height={3.17}
+            align="center"
+            style={{
+              'vertical-align': 'middle',
+              'horizontal-align': 'middle',
+            }}>
+            <Box
+              style={{
+                position: 'relative', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)',
+              }}>
+              {message.text}
+            </Box>
+          </TypedNoticeBox>
+        )}
+      </Section>
+      <Section title="Cloning Pod Status">
+        <LabeledList>
+          <LabeledList.Item label="Completion">
+            {!podGone && (
+              <ProgressBar
+                value={completion}
+                maxValue={100}
+                minValue={0}
+                ranges={{
+                  good: [90, Infinity],
+                  average: [25, 90],
+                  bad: [-Infinity, 25],
+                }} />
+            )}
+            {!!podGone && (
+              "No Pod Detected"
+            )}
+          </LabeledList.Item>
+          <LabeledList.Item label="Bio-Matter">
+            {!podGone && (
+              <ProgressBar
+                value={meatLevels}
+                maxValue={100}
+                minValue={0}
+                ranges={{
+                  good: [50, 100],
+                  average: [25, 50],
+                  bad: [0, 25],
+                }} />
+            )}
+            {!!podGone && (
+              "No Pod Detected"
+            )}
+          </LabeledList.Item>
+        </LabeledList>
+      </Section>
+      <Section title="Scanner Controls"
+        buttons={
+          <Button
+            width={7}
+            icon={scannerLocked ? "unlock" : "lock-open"}
+            align={"center"}
+            color={scannerLocked ? "bad" : "good"}
+            onClick={() => act("toggleLock")}>
+            {scannerLocked ? "Locked" : "Unlocked"}
+          </Button>
+        }>
+        {(!!scannerGone || !!occupantScanned || !scannerOccupied) && (
+          <Box>
+            <Icon color={(scannerGone || !scannerOccupied) ? "bad" : "good"}
+              name={(scannerGone || !scannerOccupied) ? "times" : "check"} />
+            {scannerGone ? " No scanner detected."
+              : !scannerOccupied ? " Scanner has no occupant" : " Occupant scanned."}
+          </Box>
+        )}
+        {(!scannerGone && !occupantScanned && scannerOccupied) && (
           <Button
             width={scannerGone ? 8 : 7}
             icon="dna"
@@ -300,42 +352,11 @@ const StatusSection = (props, context) => {
             color={scannerGone ? "bad" : "good"}
             disabled={occupantScanned | scannerGone}
             onClick={() => act("scan")}>
-            {(occupantScanned ? "Scanned" : (scannerGone ? "No Scanner" : "Scan"))}
+            Scan
           </Button>
-          <Button
-            width={7}
-            icon={scannerLocked ? "unlock" : "lock-open"}
-            align={"center"}
-            disabled={!scannerOccupied}
-            color={scannerLocked ? "bad" : "good"}
-            onClick={() => act("toggleLock")}>
-            {scannerLocked ? "Locked" : "Unlocked"}
-          </Button>
-        </Flex.Item>
-        <Flex.Item width={25} mt={2} height={1} ml={7.7}>
-          {message.text && (
-            <TypedNoticeBox
-              type={message.status}
-              textColor="white"
-              width={25.45}
-              height={3.17}
-              align="center"
-              style={{
-                'vertical-align': 'middle',
-                'horizontal-align': 'middle',
-              }}>
-              <Box
-                style={{
-                  position: 'relative', left: '50%', top: '50%',
-                  transform: 'translate(-50%, -50%)',
-                }}>
-                {message.text}
-              </Box>
-            </TypedNoticeBox>
-          )}
-        </Flex.Item>
-      </Flex>
-    </Section>
+        )}
+      </Section>
+    </Fragment>
   );
 };
 
@@ -353,7 +374,8 @@ const Records = (props, context) => {
 
   return (
     <Fragment>
-      <Section mb={0}>
+      <Section mb={0} title="Records"
+        style={{ 'border-bottom': '2px solid rgba(51, 51, 51, 0.4);' }}>
         <Flex className="cloning-console__flex__head">
           <Flex.Item className="cloning-console__head__row" mr={2}>
             <Flex.Item className="cloning-console__head__item"
@@ -362,10 +384,18 @@ const Records = (props, context) => {
             </Flex.Item>
             <Flex.Item className="cloning-console__head__item"
               style={{ 'width': '160px' }}>
-              Damage
+              <Box>Damage</Box>
+              <Box
+                style={{
+                  position: 'absolute', left: '50%', top: '50%',
+                  transform: 'translate(-40%, 22px)',
+                }}
+                fontSize="9px">
+                {"OXY / TOX / BURN / BRUTE"}
+              </Box>
             </Flex.Item>
             <Flex.Item className="cloning-console__head__item"
-              style={{ 'width': '205px' }}>
+              style={{ 'width': '155px' }}>
               Actions
             </Flex.Item>
           </Flex.Item>
@@ -380,7 +410,7 @@ const Records = (props, context) => {
               {records.map(record => (
                 <Flex.Item key={record.id} className="cloning-console__body__row">
                   <Flex.Item inline className="cloning-console__body__item"
-                    style={{ 'width': '190px', 'height': '15px' }}>
+                    style={{ 'width': '190px' }}>
                     {record.name}
                   </Flex.Item>
                   <Flex.Item
@@ -393,7 +423,7 @@ const Records = (props, context) => {
                         record.health.TOX,
                         record.health.BURN,
                         record.health.BRUTE)} />
-                    {record.implant && record.health.OXY === null ? (
+                    {(record.implant && record.health.OXY >= 0) ? (
                       <Box inline>
                         <HealthStat inline align="center" type="oxy" width={2}>
                           {shortenNumber(record.health.OXY)}
@@ -415,25 +445,33 @@ const Records = (props, context) => {
                       "No Implant Detected"
                     )}
                   </Flex.Item>
-                  <Flex.Item className="cloning-console__body__item"
-                    style={{ 'width': '205px' }}>
+                  <Flex.Item align="baseline" className="cloning-console__body__item"
+                    style={{ 'width': '155px' }}>
                     {(!!allowedToDelete && (
                       <Button
                         icon="trash"
                         color={"bad"}
                         onClick={() =>
                         { setDeletionTarget(record.ckey);
-                        }}>
-                        Delete
-                      </Button>
+                        }} />
                     ))}
                     {(!!disk && (
                       <Button
-                        icon="save"
+                        icon={(!diskReadOnly && record.saved) ? "" : "save"}
                         color={"blue"}
+                        alignText="center"
+                        width="22px"
                         disabled={record.saved || diskReadOnly}
                         onClick={() => act("saveToDisk", { ckey: record.ckey })}>
-                        {record.saved ? (diskReadOnly ? "Read Only" : "Saved") : (diskReadOnly ? "Read Only" : "Save")}
+                        {(!!record.saved && (
+                          <Icon color="black" name="check" />
+                        ))}
+                        {(!!diskReadOnly && (
+                          <Icon.Stack>
+                            <Icon color="black" name="pen" />
+                            <Icon color="black" name="slash" />
+                          </Icon.Stack>
+                        ))}
                       </Button>
                     ))}
                     <Button
