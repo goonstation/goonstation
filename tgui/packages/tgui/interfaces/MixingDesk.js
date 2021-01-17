75c742f8a92c8a36ae3debb691999fe11a0764ef
pali <6pali6@gmail.com>
2021-01-17 13:28:27 +0100

    converted mixing desk to tgui

---
+++ tgui/packages/tgui/interfaces/MixingDesk.js
@@ -0,0 +1,108 @@
+import { useBackend, useSharedState } from '../backend';
+import { Button, LabeledList, Section, Divider, Modal, Input } from '../components';
+import { Window } from '../layouts';
+
+export const MixingDesk = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    voices,
+    selected_voice,
+    say_popup,
+  } = data;
+  const [message, setMessage] = useSharedState(context, "message", null);
+
+  const sayPopup = () => (
+    <Modal>
+      Choose something to say as {
+        selected_voice > 0 && selected_voice <= voices.length
+          ? voices[selected_voice - 1].name
+          : "yourself"
+      }:
+      <br />
+      <Input
+        autoFocus
+        selfClear
+        width={20}
+        value={message}
+        onEnter={(_, msg) => {
+          window.focus();
+          act("say", { "message": msg });
+          setMessage("");
+        }}
+        onChange={(_, msg) => setMessage(msg)} />
+      <br />
+      <Button onClick={() => { act("say", { "message": message }); setMessage(""); }}>Say</Button>
+      <Button onClick={() => { act("cancel_say"); setMessage(""); }}>Cancel</Button>
+    </Modal>);
+
+  const onKeyDown = e => {
+    let key = String.fromCharCode(e.keyCode);
+    let caught_key = true;
+    if (key === "T") {
+      act("say_popup");
+    }
+    else if (e.keyCode === 27 && say_popup) { // escape
+      act("cancel_say");
+      setMessage("");
+    }
+    else if (!say_popup) {
+      let num = Number(key);
+      if (String(num) === key) {
+        // apparently in js this is the correct way to check if it's a number
+        act("switch_voice", { id: num });
+      }
+      else {
+        caught_key = false;
+      }
+    }
+    else {
+      caught_key = false;
+    }
+    if (caught_key) {
+      e.stopPropagation();
+    }
+  };
+
+  const ellipsis = text => text.length > 18 ? text.substring(0, 18) + "…" : text;
+
+  return (
+    <Window
+      height={375}
+      width={370}>
+      <Window.Content
+        onkeydown={onKeyDown}>
+        {!!say_popup && sayPopup()}
+        <Section
+          title="Voice Synthesizer">
+          <Divider />
+          <LabeledList>
+            { voices.map((entry, id) => (
+              <LabeledList.Item
+                key={entry["name"]}
+                label={(id + 1) + " " + (entry["accent"] ? ellipsis(entry["name"]) + " [" + entry["accent"] + "]" : ellipsis(entry["name"]))}
+                labelColor={id + 1 === selected_voice ? "red" : "label"}>
+                <Button
+                  icon="trash-alt"
+                  onClick={() => act("remove_voice", { id: id + 1 })} />
+                <Button
+                  icon="bullhorn"
+                  onClick={() => act("say_popup", { id: id + 1 })} />
+              </LabeledList.Item>
+            )) }
+            <LabeledList.Item>
+              <Button
+                icon="plus"
+                onClick={() => act("add_voice")}
+                disabled={voices.length >= 9} />
+              <Button
+                position="relative"
+                icon="question-circle"
+                tooltip="Press T to talk and 1-9 keys to switch voices. Press 0 to reset to your normal voice."
+                tooltipPosition="right" />
+            </LabeledList.Item>
+          </LabeledList>
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
