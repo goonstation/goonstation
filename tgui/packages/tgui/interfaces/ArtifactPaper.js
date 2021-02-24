6e36d9f8ca76fcbb75f8a39738b74488846cc212
zjdtmkhzt <bardlover69@gmail.com>
2021-02-24 14:01:56 +0100

    add the weird and scary tgui files

--- tgui/packages/tgui/interfaces/ArtifactPaper.js
+++ tgui/packages/tgui/interfaces/ArtifactPaper.js
@@ -14,71 +14,71 @@ export const ArtifactPaper = (props, context) => {
     allArtifactOrigins,
     allArtifactTypes,
     allArtifactTriggers,
-    hasPen
+    hasPen,
   } = data;
   return (
     <Window
-    resizable
-    title="Nanotrasen Alien Artifact Analysis Form"
-    theme="paper"
-    width={800}
-    height={830}
+      resizable
+      title="Nanotrasen Alien Artifact Analysis Form"
+      theme="paper"
+      width={800}
+      height={830}
     >
-        <Section title="Artifact Data">
-            <h3>Artifact Name</h3>
-            <span>{artifactName}</span>
-            <h3>Artifact Origin</h3>
-            <Flex direction={"column"} wrap={"wrap"} height={3}>
-              {allArtifactOrigins.map(x => (
-              <Flex.Item
-                onClick={(e, value) => act("origin", {newOrigin: x, hasPen: hasPen})}>
-                <Button.Checkbox
-                  checked={artifactOrigin == x}
-                />
-                <a>{x}</a>
-              </Flex.Item>
-              ))}
-            </Flex>
-            <h3>Artifact Type</h3>
-            <Flex direction={"column"} wrap={"wrap"} height={25} justify={"space-evenly"}>
-              {allArtifactTypes.map(atype => (
-              <Flex.Item
-               onClick={(e, value) => act("type", {newType: atype, hasPen: hasPen})}>
-               <Button.Checkbox
-                checked={artifactType == atype}
-               />
-               <a>{atype}</a>
-              </Flex.Item>
-              ))}
-            </Flex>
-            <h3>Artifact Triggers</h3>
-            <Flex direction={"column"} wrap={"wrap"} height={5}>
-              {allArtifactTriggers.map(x => (
-              <Flex.Item
-                onClick={(e, value) => act("trigger", {newTriggers: x, hasPen: hasPen})}>
-                <Button.Checkbox
-                  checked={artifactTriggers == x}
-                />
-                <a>{x}</a>
-              </Flex.Item>
-              ))}
-            </Flex>
-            <h3>Artifact Faults</h3>
-              <TextArea
-                value={artifactFaults}
-                backgroundColor="#DDDDDD"
-                fluid={true}
-                height={5}
-                onChange={(_, x) => act("fault", { newFaults: x, hasPen: hasPen})} />
+      <Section title="Artifact Data">
+        <h3>Artifact Name</h3>
+        <span height={500}>{artifactName}</span>
+        <h3>Artifact Origin</h3>
+        <Flex direction={"column"} wrap={"wrap"} height={3}>
+          {allArtifactOrigins.map(x => (
+            <Flex.Item key={x.id}
+              onClick={(e, value) => act("origin", { newOrigin: x, hasPen: hasPen })}>
+              <Button.Checkbox
+                checked={artifactOrigin === x}
+              />
+              <a>{x}</a>
+            </Flex.Item>
+          ))}
+        </Flex>
+        <h3>Artifact Type</h3>
+        <Flex direction={"column"} wrap={"wrap"} height={25} justify={"space-evenly"}>
+          {allArtifactTypes.map(x => (
+            <Flex.Item key={x.id}
+              onClick={(e, value) => act("type", { newType: x, hasPen: hasPen })}>
+              <Button.Checkbox
+                checked={artifactType === x}
+              />
+              <a>{x}</a>
+            </Flex.Item>
+          ))}
+        </Flex>
+        <h3>Artifact Triggers</h3>
+        <Flex direction={"column"} wrap={"wrap"} height={5}>
+          {allArtifactTriggers.map(x => (
+            <Flex.Item key={x.id}
+              onClick={(e, value) => act("trigger", { newTriggers: x, hasPen: hasPen })}>
+              <Button.Checkbox
+                checked={artifactTriggers === x}
+              />
+              <a>{x}</a>
+            </Flex.Item>
+          ))}
+        </Flex>
+        <h3>Artifact Faults</h3>
+        <TextArea
+          value={artifactFaults}
+          backgroundColor="#DDDDDD"
+          fluid
+          height={5}
+          onChange={(_, x) => act("fault", { newFaults: x, hasPen: hasPen })} />
 
-            <h3>Additioal Information</h3>
-            <TextArea
-              value={artifactDetails}
-              backgroundColor="#DDDDDD"
-              fluid={true}
-              height={10}
-              onChange={(_, x) => act("detail", { newDetail: x, hasPen: hasPen})} />
-        </Section>
+        <h3>Additional Information</h3>
+        <TextArea
+          value={artifactDetails}
+          backgroundColor="#DDDDDD"
+          fluid
+          height={10}
+          onChange={(_, x) => act("detail", { newDetail: x, hasPen: hasPen })} />
+      </Section>
     </Window>
   );
 };
