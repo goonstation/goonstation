0e24e093ea9f96000cdc7c3898de47e19cd4f407
ZeWaka <zewakagamer@gmail.com>
2021-04-19 19:57:25 -0700

    maybe fix stupid newlines

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 DreamDaemon goonstation.dmb -once -quiet -close -trusted -verbose -invisible
+touch ./no_runtimes.txt
+sed -i '/^[[:space:]]*$/d' ./no_runtimes.txt
 if [ -s ./no_runtimes.txt ]
 then
 	cat ./no_runtimes.txt
