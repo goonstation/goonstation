df8076a36bdcc20c884518470e49e0fb1894459f
ZeWaka <zewakagamer@gmail.com>
2021-06-11 18:08:46 -0700

    okay this should actually be it

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+touch errors.log
 DreamDaemon goonstation.dmb -once -quiet -close -trusted -verbose -invisible
-ls
-cd data/logs
-ls
-cd ..
-ls
-if [ ! -s ./errors.log ]
+if [ -s "errors.log" ]
 then
 	echo "Errors detected!"
 	sed -i '/^[[:space:]]*$/d' ./errors.log
-	cat ./errors.log
+	cat errors.log
 	exit 1
 else
 	echo "No errors detected."
