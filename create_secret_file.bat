18ddf82089c0089fc468838463571178cc289472
zjdtmkhzt <bardlover69@gmail.com>
2021-12-28 23:35:14 +0100

    updated batch file to create secret file

--- create_secret_file.bat
+++ create_secret_file.bat
@@ -1,5 +1,6 @@
 @echo off
 cd +secret
+if exist __secret.dme (echo Secret File already exists & pause & exit) else (echo Attempting to create Secret File)
 echo[ > __secret.dme
-if exist __secret.dme (echo Secret File successfully created) else (echo There was a problem with creating the secret file)
+if exist __secret.dme (echo Secret File successfully created) else (echo There was a problem with creating the Secret File)
 pause
