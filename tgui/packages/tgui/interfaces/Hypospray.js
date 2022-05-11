a58eae08ba05b5ac9a6f3e9788fc5feceb9bfc62
codeJester <45642475+codeJester27@users.noreply.github.com>
2022-05-11 04:27:22 -0500

    everything and bundle

---
+++ tgui/packages/tgui/interfaces/Hypospray.js
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @copyright 2022
+ * @author CodeJester (https://github.com/codeJester27)
+ * @license ISC
+ */
+
+import { useBackend } from "../backend";
+import { Button, Section, Slider } from "../components";
+import { Window } from '../layouts';
+import { ReagentGraph, ReagentList } from './common/ReagentInfo';
+
+export const Hypospray = (_props, context) => {
+  const { act, data } = useBackend(context);
+  const { emagged, injectionAmount, reagentData } = data;
+
+  return (
+    <Window
+      width={320}
+      height={300}
+      theme={emagged ? "syndicate" : "nanotrasen"}>
+      <Window.Content>
+        <Section title="Contents"
+          buttons={
+            <Button
+              icon="times"
+              color="red"
+              disabled={!reagentData.totalVolume}
+              onClick={() => act('dump')}
+            >
+              Dump
+            </Button>
+          }>
+          <ReagentGraph container={reagentData} />
+          <ReagentList container={reagentData} />
+        </Section>
+        <Section title="Injection Amount">
+          <Slider
+            value={injectionAmount}
+            format={value => value + "u"}
+            minValue={1}
+            maxValue={30}
+            step={1}
+            stepPixelSize={10}
+            onChange={(e, value) => act('changeAmount', { amount: value })}
+          />
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
