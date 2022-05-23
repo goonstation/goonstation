73410ae5611031346db1b50cc7de4f72b78c4c5c
skeletonman0 <skeletonman0a@gmail.com>
2022-05-23 17:47:51 -0400

    some changes

--- tgui/packages/tgui/interfaces/Terminal.js
+++ tgui/packages/tgui/interfaces/Terminal.js
@@ -1,78 +1,78 @@
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
-import { Box, Input, Button, Section } from 'tgui/components';
-import { ColorBox, Flex } from '../components';
+import { Box, Button, Flex, Input, Section, Stack } from '../components';
 
 export const Terminal = (props, context) => {
   const { act, data } = useBackend(context);
   const peripherals = data.peripherals || [];
-  const { textInput } = props;
+  const { textInput } = "";
 
   const {
-    fdisk,
-    idcard,
     displayHTML,
     TermActive,
     windowName,
-    user,
     fontColor,
     bgColor,
-    diskdrive,
   } = data;
 
   return (
     <Window
       theme="retro-dark"
       title={windowName}
-      width="380"
       fontFamily="Consolas"
-      font-size="10pt"
+      width="380"
       height="350">
       <Window.Content>
-        <Section backgroundColor="#0f0f0f" scrollable fill height="70%" >
-          <Box color={fontColor} backgroundColor={bgColor} minHeight="99%" maxHeight="500em">
-            <Box mx="1%"
-              fontFamily="Consolas"
-              fill
-              color={fontColor}
-              backgroundColor={bgColor}
-              dangerouslySetInnerHTML={{ __html: displayHTML }}
-            />
-          </Box>
-        </Section>
-        <Section mt="1%" fitted>
-          <Input
-            as="span"
-            placeholder="Type Here"
-            selfClear
-            value={textInput}
-            width="80%"
-            mt="1%"
-            ml="1%"
-            onChange={(e, value) => act('text', { value: value })}
-          />
-          <Button ml="11%" icon="power-off"
-            color={data.TermActive ? "green" : "red"}
-            onClick={() => act('restart')} />
-        </Section>
-        <Section fitted fill height="20%">
-          {peripherals.map(peripheral => {
-            return (
-              <Button
-                ml="1%"
-                mt="8%"
-                mb="5%"
-                key={peripheral.card}
-                icon={peripheral.icon}
-                content={peripheral.label}
-                fontFamily={peripheral.Clown ? "Comic Sans MS" : "Consolas"}
-                color={peripheral.color ? "green" : "grey"}
-                onClick={() => act('buttonPressed', {
-                  card: peripheral.card })}
+        <Stack vertical fill>
+          <Stack.Item grow>
+            <Section backgroundColor={bgColor} scrollable fill>
+              <Box
+                fontFamily="Consolas"
+                fill
+                color={fontColor}
+                dangerouslySetInnerHTML={{ __html: displayHTML }}
               />
-            );
-          })}
-        </Section>
+            </Section>
+          </Stack.Item>
+          <Stack.Item>
+            <Section fitted>
+              <Flex>
+                <Flex.Item grow>
+                  <Input
+                    as="span"
+                    placeholder="Type Here"
+                    selfClear
+                    value={textInput}
+                    fluid
+                    onChange={(e, value) => act('text', { value: value })}
+                  />
+                </Flex.Item>
+                <Flex.Item>
+                  <Button icon="power-off"
+                    color={TermActive ? "green" : "red"}
+                    onClick={() => act('restart')} />
+                </Flex.Item>
+              </Flex>
+            </Section>
+          </Stack.Item>
+          <Stack.Item>
+            <Section fitted>
+              {peripherals.map(peripheral => {
+                return (
+                  <Button
+                    key={peripheral.card}
+                    icon={peripheral.icon}
+                    content={peripheral.label}
+                    fontFamily={peripheral.Clown ? "Comic Sans MS" : "Consolas"}
+                    color={peripheral.color ? "green" : "grey"}
+                    onClick={() => act('buttonPressed', {
+                      card: peripheral.card })}
+                  />
+                );
+              })}
+            </Section>
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
