1ee5439068e3ef7ec460feef35e5c19141011921
ZeWaka <zewakagamer@gmail.com>
2022-05-30 23:52:17 -0700

    stats tgui work

--- tgui/packages/tgui/interfaces/ReactorStats.js
+++ tgui/packages/tgui/interfaces/ReactorStats.js
@@ -7,8 +7,36 @@
 
 import { useBackend } from '../backend';
 import { Box, Chart, LabeledList, Section, Divider } from '../components';
-import { formatPower, formatSiUnit } from '../format';
 import { Window } from '../layouts';
+
+/**
+ * Helper function to transform the data into something displayable
+ * Lovingly made by Mordent
+ * @param {*} statsData - [ { foo: 1, bar: 2 }, { foo: 3, bar: 4 } ]
+ * @returns - { foo: [1, 3], bar: [2, 4] }
+ */
+const processStatsData = statsData => {
+  if ((statsData ?? []).length === 0) {
+    return null; // or if you know the keys in advance, can return a constant with them having empty arrays as values
+  }
+  // intialize our data structure
+  const keys = Object.keys(statsData[0]);
+
+  const resolvedData = keys.reduce((acc, curr) => {
+    acc[curr] = [];
+    return acc;
+  }, {});
+
+  for (let statsDataIndex = 0; statsDataIndex < statsData.length; statsDataIndex++) {
+    const tegDatum = statsData[statsDataIndex];
+    for (let keyIndex = 0; keyIndex < keys.length; keyIndex++) {
+      const key = keys[keyIndex];
+      // will insert 'undefined' if not present, if you want different handling here then I guess do it?
+      resolvedData[key].push(tegDatum[key]);
+    }
+  }
+  return resolvedData;
+};
 
 export const ReactorStats = (props, context) => {
   const { act, data } = useBackend(context);
@@ -18,11 +46,11 @@ export const ReactorStats = (props, context) => {
     chamberData,
     meterData,
   } = data;
-  const tegMap = tegData.map((value, i) => [i, value]);
-  const tegMax = Math.max(...tegData["Output"]);
 
-  const formatTemperature = temperature =>
-    `${temperature >= 1000 ? temperature.toExponential(3) : temperature} K`;
+  const tegStats = processStatsData(tegData);
+  const chamberStats = processStatsData(chamberData);
+  const meterStats = processStatsData(meterData);
+
   return (
     <Window
       height="520"
@@ -31,17 +59,22 @@ export const ReactorStats = (props, context) => {
         {!turnedOn
           ? (<Box> Unpowered </Box>)
           :(
-            <Section title="Status">
-              <LabeledList>
-                <LabeledList.Item label="Output History" />
-              </LabeledList>
-              <Chart.Line
-                height="5em"
-                data={tegMap}
-                rangeX={[0, tegMap.length - 1]}
-                rangeY={[0, tegMax]}
-                strokeColor="rgba(1, 184, 170, 1)"
-                fillColor="rgba(1, 184, 170, 0.25)" />
+            <Section title="TEG Data">
+              {tegStats.map(({ key, value: data }) => (
+                <Section key={key}>
+                  <LabeledList>
+                    <LabeledList.Item label={key} />
+                  </LabeledList>
+                  <Chart.Line
+                    key={key}
+                    height="5em"
+                    data={data}
+                    rangeX={[0, data.length - 1]}
+                    rangeY={[0, Math.max(...data)]}
+                    strokeColor="rgba(1, 184, 170, 1)"
+                    fillColor="rgba(1, 184, 170, 0.25)" />
+                </Section>
+              ))}
             </Section>
           )}
       </Window.Content>
