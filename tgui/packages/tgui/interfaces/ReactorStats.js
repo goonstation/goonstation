a5b251131e9a09f9116304da25d0b8882ee45035
ZeWaka <zewakagamer@gmail.com>
2022-06-01 23:16:44 -0700

    aaaaaaaaaaaaaaaaaa

--- tgui/packages/tgui/interfaces/ReactorStats.js
+++ tgui/packages/tgui/interfaces/ReactorStats.js
@@ -31,8 +31,8 @@ const processStatsData = statsData => {
     const tegDatum = statsData[statsDataIndex];
     for (let keyIndex = 0; keyIndex < keys.length; keyIndex++) {
       const key = keys[keyIndex];
-      // will insert 'undefined' if not present, if you want different handling here then I guess do it?
-      resolvedData[key].push(tegDatum[key] ?? 0);
+      // x,y coords for graph (y defaults to 0)
+      resolvedData[key].push([statsDataIndex, tegDatum[key] ?? 0]);
     }
   }
   return resolvedData;
@@ -51,30 +51,47 @@ export const ReactorStats = (props, context) => {
   const chamberStats = processStatsData(chamberData);
   // const meterStats = processStatsData(meterData);
 
+  // eslint-disable-next-line max-len
+  const boob = [[0, 66780.8], [1, 66680.6], [2, 66590.5], [3, 66510.1], [4, 66417.1], [5, 66256.9], [6, 66068.4], [7, 65906.3], [8, 65754], [9, 65621.8], [10, 65514.2], [11, 65423.9], [12, 65357.2], [13, 65305.9], [14, 65279.7], [15, 65264.6], [16, 65262.4], [17, 65266.3], [18, 65286.2], [19, 65310], [20, 65339.6], [21, 65382.2], [22, 65428.8], [23, 65483.6], [24, 65546.8], [25, 65620.4], [26, 65700.7], [27, 65788.1], [28, 65888.1], [29, 65999.2], [30, 66116.2], [31, 66243.8], [32, 66380.6], [33, 66526.5], [34, 66679.1], [35, 66683.2], [36, 66375.9], [37, 66078.3], [38, 65795.6], [39, 65521.4], [40, 65263.5], [41, 65013.9], [42, 64771.6], [43, 64545.1], [44, 64323.9], [45, 64115.5], [46, 63912.4], [47, 63717], [48, 63525.9], [49, 63374.7]];
+
+  const tegCharts = Object.entries(tegStats).map(([key, chart_data], _index) => (
+    <Box key={key}>
+      <LabeledList>
+        <LabeledList.Item label={key}>
+          {chart_data[chart_data.length -1]}
+        </LabeledList.Item>
+      </LabeledList>
+      <Chart.Line
+        height="5em"
+        data={chart_data}
+        rangeX={[0, data.length - 1]}
+        rangeY={[0, Math.max(...data)]}
+        strokeColor="rgba(1, 184, 170, 1)"
+        fillColor="rgba(1, 184, 170, 0.25)" />
+    </Box>
+  ));
+
+  const historyData = boob.map((value, i) => [i, value]);
+  const historyMax = Math.max(...boob);
+
   return (
     <Window
       height="520"
-      width="300" >
+      width="600"
+      scrollable>
       <Window.Content>
         {!turnedOn
           ? (<Box> Unpowered </Box>)
           :(
             <Section title="TEG Data">
-              {tegStats.map(({ key, value: data }) => (
-                <Section key={key}>
-                  <LabeledList>
-                    <LabeledList.Item label={key} />
-                  </LabeledList>
-                  <Chart.Line
-                    key={key}
-                    height="5em"
-                    data={data}
-                    rangeX={[0, data.length - 1]}
-                    rangeY={[0, Math.max(...data)]}
-                    strokeColor="rgba(1, 184, 170, 1)"
-                    fillColor="rgba(1, 184, 170, 0.25)" />
-                </Section>
-              ))}
+              <Chart.Line
+                height="5em"
+                data={historyData}
+                rangeX={[0, historyData.length - 1]}
+                rangeY={[0, historyMax]}
+                strokeColor="rgba(1, 184, 170, 1)"
+                fillColor="rgba(1, 184, 170, 0.25)" />
+              { tegCharts }
             </Section>
           )}
       </Window.Content>
