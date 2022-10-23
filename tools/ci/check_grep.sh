eeaacf37018f7ca2e873b4674d5d87a1a694e07f
ZeWaka <zewakagamer@gmail.com>
2022-10-23 16:43:55 -0700

    TGM map format grep check re-enabled

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -7,10 +7,10 @@ shopt -s globstar
 
 st=0
 
-#if grep -El '^\".+\" = \(.+\)' maps/**/*.dmm;	then
-#    echo "ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!"
-#    st=1
-#fi;
+if grep -El '^\".+\" = \(.+\)' maps/**/*.dmm;	then
+   echo "ERROR: Non-TGM formatted map detected. Please convert it using Map Merger or StrongDMM!"
+   st=1
+fi;
 if grep -P 'step_[xy]' maps/**/*.dmm;	then
     echo "ERROR: step_x/step_y variables detected in maps, please remove them."
     st=1
