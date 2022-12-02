1ae765777a2c87ab6c2bd23e79da2430b58de4fa
ZeWaka <zewakagamer@gmail.com>
2022-12-01 23:04:51 -0800

    working install git hook command for windows

--- tgui/bin/tgui_.ps1
+++ tgui/bin/tgui_.ps1
@@ -60,6 +60,16 @@ function task-lint {
   Write-Output "tgui: eslint check passed"
 }
 
+## Installs merge drivers and git hooks
+function task-install-git-hooks() {
+  Set-Location $basedir
+  $git_root = "$(git rev-parse --show-toplevel)"
+  $git_base_dir = "${basedir}/${git_root}/.}"
+  git config --replace-all merge.tgui-merge-bundle.driver \
+    "${git_base_dir}/bin/tgui --merge=bundle %O %A %B %L %P"
+  Write-Output "tgui: Merge drivers have been successfully installed!"
+}
+
 function task-test {
   yarn run jest
 }
@@ -139,6 +149,12 @@ if ($Args.Length -gt 0) {
   if ($Args[0] -eq "--analyze") {
     task-install
     task-webpack --mode=production --analyze
+    exit 0
+  }
+
+  ## Hook install
+  if ($Args[0] -eq "--install-git-hooks") {
+    task-install-git-hooks
     exit 0
   }
 }
