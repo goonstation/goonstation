de79afd9a8a513fde7f44acdaae652219545305d
DisturbHerb <gutsgreenwich@gmail.com>
2023-02-27 23:27:37 +1000

    implemented cog's feedback best i can

--- tgui/packages/tgui/interfaces/CrewCredits.tsx
+++ tgui/packages/tgui/interfaces/CrewCredits.tsx
@@ -1,20 +1,19 @@
 import { BooleanLike } from "common/react";
 import { useBackend } from "../backend";
-import { Box, Section } from "../components";
+import { Icon, Section, Stack } from "../components";
 import { Window } from "../layouts";
 
 interface CrewCreditsData {
-  darkMode: BooleanLike,
   groups: GroupBlockProps[]
 }
 
 export const CrewCredits = (props, context) => {
   const { data } = useBackend<CrewCreditsData>(context);
   return (
-    <Window title="Crew Credits" width={600} height={600}>
+    <Window title="Crew Credits" width={500} height={500}>
       <Window.Content scrollable>
         {data.groups?.map((group, index) =>
-          (!!(data.groups[index].crew.length > 0) && <GroupBlock key={index} group={group.group} crew={group.crew} />)
+          ((data.groups[index].crew.length > 0) && <GroupBlock key={index} group={group.group} crew={group.crew} />)
         )}
       </Window.Content>
     </Window>);
@@ -26,31 +25,34 @@ interface GroupBlockProps {
 }
 
 const GroupBlock = (props:GroupBlockProps) => {
-  const { group, crew } = props;
-  const group_title = group;
+  const { group: group_title, crew } = props;
+  const dep_heads = crew?.filter(member => member.head);
+
   return (
     <Section title={group_title}>
-      {crew?.map((member, index) =>
-        (
-          !!member.head && <CrewMember
-            key={"head" + index}
-            real_name={member.real_name}
-            dead={member.dead}
-            player={member.player}
-            role={member.role}
-            head />
-        )
-      )}
-      {crew?.map((member, index) =>
-        (
-          !member.head && <CrewMember
-            key={index}
-            real_name={member.real_name}
-            dead={member.dead}
-            player={member.player}
-            role={member.role} />
-        )
-      )}
+      <Stack fill vertical>
+        {dep_heads?.map((member, index) =>
+          (
+            <CrewMember
+              key={"head" + index}
+              real_name={member.real_name}
+              dead={member.dead}
+              player={member.player}
+              role={member.role}
+              head />
+          )
+        )}
+        {crew?.map((member, index) =>
+          (
+            !member.head && <CrewMember
+              key={index}
+              real_name={member.real_name}
+              dead={member.dead}
+              player={member.player}
+              role={member.role} />
+          )
+        )}
+      </Stack>
     </Section>
   );
 
@@ -67,9 +69,11 @@ interface CrewMemberProps {
 const CrewMember = (props: CrewMemberProps) => {
   const { real_name, dead, player, role, head } = props;
   return (
-    <>
-      <Box as="span" bold={head}>{real_name} {!!dead && "[DEAD]"} (played by {player}) as {role}</Box>
-      <br />
-    </>
+    <Stack.Item>
+      <Stack fill bold={head} justify="space-between">
+        <Stack.Item maxWidth="30%">{role}</Stack.Item>
+        <Stack.Item grow textAlign="right">{!!dead && <Icon name="skull" />} {real_name} (played by {player})</Stack.Item>
+      </Stack>
+    </Stack.Item>
   );
 };
