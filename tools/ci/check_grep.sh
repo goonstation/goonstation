6a7888b833773e9af59ab66ee34212744e97c96e
ZeWaka <zewakagamer@gmail.com>
2023-09-22 17:17:36 +0000

    better and not shit

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -38,7 +38,7 @@ if grep -P 'playsound\(([^,]*), "(sound\/[^\[]+)"' code/**/*.dm;	then
     st=1
 fi;
 
-if grep -P '[^.]plane = [0-9]+' */**/*.dm;	then
+if grep -P 'plane\s*=\s*[0-9]+|plane\s*=\s*[A-Z_]+\s*[+\-*]\s*' */**/*.dm;	then
     echo "ERROR: don't directly set plane to a number, please use a define."
     st=1
 fi;
