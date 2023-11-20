f87ee67522a4f76188153a711f8476b97584d7dd
Cheffie <113442598+CheffieGithub@users.noreply.github.com>
2023-11-20 20:48:11 +0000

    remove left over logs

--- tgui/packages/tgui-panel/context/reducer.js
+++ tgui/packages/tgui-panel/context/reducer.js
@@ -7,9 +7,6 @@
  */
 
 import { setContextFlags, setContextName, setContextTarget } from './actions';
-import { createLogger } from 'tgui/logging';
-
-const logger = createLogger('REDUCER');
 
 const initialState = {
   contextTarget: null,
@@ -20,21 +17,18 @@ const initialState = {
 export const contextReducer = (state = initialState, action) => {
   const { type, payload, meta } = action;
   if (type === setContextTarget.type) {
-    logger.log(payload);
     return {
       ...state,
       contextTarget: payload,
     };
   }
   if (type === setContextFlags.type) {
-    logger.log(payload);
     return {
       ...state,
       contextFlags: payload,
     };
   }
   if (type === setContextName.type) {
-    logger.log(payload);
     return {
       ...state,
       contextName: payload,
