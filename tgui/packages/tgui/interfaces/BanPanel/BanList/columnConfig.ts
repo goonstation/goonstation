0474328ec640d9ec5b1a651f8846832789680a35
ZeWaka <zewakagamer@gmail.com>
2023-11-22 22:55:26 -0800

    column shit

--- tgui/packages/tgui/interfaces/BanPanel/BanList/columnConfig.ts
+++ tgui/packages/tgui/interfaces/BanPanel/BanList/columnConfig.ts
@@ -6,7 +6,31 @@ export const columnConfigs: ColumnConfig<BanResource>[] = [
     header: 'ID',
     id: 'id',
     getValue: (data) => data.id,
+    basis: 4,
+  },
+  {
+    header: 'ckey',
+    id: 'ckey',
+    getValue: (data) => data.original_ban_detail.ckey,
+    basis: 15, // I think 32 chars is the max, this is slightly below but whatever
+  },
+  {
+    header: 'Time',
+    id: 'time',
+    getValue: (data) => data.expires_at,
+    basis: 7,
+  },
+  {
+    header: 'Server',
+    id: 'server',
+    getValue: (data) => data.server_id ?? "All",
     basis: 5,
+  },
+  {
+    header: 'Admin',
+    id: 'admin',
+    getValue: (data) => data.game_admin.ckey,
+    basis: 7,
   },
   {
     header: 'Reason',
@@ -14,5 +38,17 @@ export const columnConfigs: ColumnConfig<BanResource>[] = [
     getValue: (data) => data.reason,
     basis: 5,
     grow: 1,
+  },
+  {
+    header: 'CID',
+    id: 'cid',
+    getValue: (data) => data.original_ban_detail.comp_id,
+    basis: 7,
+  },
+  {
+    header: 'IP',
+    id: 'ip',
+    getValue: (data) => data.original_ban_detail.ip,
+    basis: 9,
   },
 ];
