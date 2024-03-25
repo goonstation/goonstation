f00a4a7d1f90fd88a5b08457fead6dcc61b81c9f
pgmzeta <ryan@crystal.surf>
2024-03-25 16:24:38 -0700

    TGUI Transception array

---
+++ tgui/packages/tgui/interfaces/TransceptionInterlink/type.ts
@@ -0,0 +1,12 @@
+export interface TransceptionInterlinkData {
+  pads: PadData[],
+  crate_count: number
+}
+
+export interface PadData {
+  device_index: string, // key in the interlink's manifest list
+  identifier: string, // unique text identifier of the pad
+  target_id: string, // the pad's network ID (i.e. target pad)
+  location: string, // location (area) of the pad
+  array_link: string, // status code of the pad
+}
