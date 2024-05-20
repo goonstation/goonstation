417710545523894b8e9073793918e2a347eb1916
Romayne <27376947+MeggalBozale@users.noreply.github.com>
2024-05-20 15:33:07 -0400

    there we are

---
+++ tgui/packages/tgui/interfaces/Manufacturer/components/ManufacturerSettings.tsx
@@ -0,0 +1,79 @@
+/**
+ * @file
+ * @copyright 2024
+ * @author Romayne (https://github.com/MeggalBozale)
+ * @license ISC
+ */
+
+import { Button, LabeledList, Section, Slider, Stack } from "../../../components";
+
+type ManufacturerSettingsProps = {
+  repeat: boolean;
+  hacked: boolean;
+  speed: number;
+  max_speed_normal: number;
+  max_speed_hacked:number;
+  mode: 'working' | 'halt' | 'ready';
+  actionSetSpeed: (speed:number) => void;
+  actionRepeat: () => void;
+};
+
+export const ManufacturerSettings = (props:ManufacturerSettingsProps) => {
+  const {
+    repeat,
+    hacked,
+    speed,
+    max_speed_normal,
+    max_speed_hacked,
+    mode,
+    actionSetSpeed,
+    actionRepeat,
+  } = props;
+  return (
+    <Stack.Item>
+      <Section
+        textAlign="center"
+        title="Fabricator Settings"
+      >
+        <LabeledList>
+          <LabeledList.Item
+            label="Repeat"
+            buttons={<Button icon="repeat" onClick={() => actionRepeat()}>Toggle Repeat</Button>}
+            textAlign="center"
+          >
+            {repeat ? "On" : "Off"}
+          </LabeledList.Item>
+          <LabeledList.Item
+            label="Speed"
+          >
+            <Stack>
+              <Stack.Item>
+                <Button
+                  icon="angle-double-left"
+                  onClick={actionSetSpeed(1)}
+                />
+              </Stack.Item>
+              <Stack.Item grow>
+                <Slider
+                  minValue={1}
+                  value={speed}
+                  maxValue={hacked ? max_speed_hacked : max_speed_normal}
+                  step={1}
+                  stepPixelSize={100}
+                  disabled={mode === "working"}
+                  onChange={(_e: any, value: number) => actionSetSpeed(value)}
+                />
+              </Stack.Item>
+              <Stack.Item>
+                <Button
+                  icon="angle-double-right"
+                  onClick={actionSetSpeed(hacked ? max_speed_hacked : max_speed_normal)}
+                />
+              </Stack.Item>
+            </Stack>
+          </LabeledList.Item>
+        </LabeledList>
+      </Section>
+    </Stack.Item>
+  );
+};
