764b7f58ac8f8979949c3325b6d3131704a8e971
Garash2k <6396368+Garash2k@users.noreply.github.com>
2024-05-23 04:16:40 -0400

    v1

--- tgui/packages/tgui/interfaces/ChemiCompiler/index.tsx
+++ tgui/packages/tgui/interfaces/ChemiCompiler/index.tsx
@@ -8,60 +8,85 @@
 import { useBackend } from '../../backend';
 import { Window } from '../../layouts';
 import { ChemiCompilerData } from './type';
-import { Button, Input } from '../../components';
+import { Button, Icon, Section, Stack, TextArea } from '../../components';
 
 export const ChemiCompiler = (_props, context) => {
   const { act, data } = useBackend<ChemiCompilerData>(context);
   const { reservoirs, buttons, inputValue, loadTimestamp, sx, tx, ax } = data;
 
   return (
-    <Window width={400} height={325}>
+    <Window width={600} height={475}>
       <Window.Content>
-        Reservoirs:<br />
-        {reservoirs.map((reservoir, index) => (
-          <Button
-            key={index}
-            onClick={() => act('reservoir', { index })}>
-            {reservoir ? "**Eject" : "None"}
-          </Button>
-        ))} <br />
+        <Stack fill>
+          <Stack.Item grow>
+            <TextArea
+              value={inputValue}
+              onInput={(_event, value) => { act('updateInputValue', { value }); }}
+              grow
+              height="100%"
+              width={'100%'}
+              // The load button would break if we pressed it between the input's act and the next refresh.
+              // This ensures a refresh after every load button click
+              key={loadTimestamp}
+              fontFamily="Consolas"
+              fontSize="13px"
+              style={{ "word-break": "break-all" }}
+            />
+          </Stack.Item>
+          <Stack.Item basis={18} textAlign="center">
+            <Section title="Reservoirs">
+              <Stack wrap justify="center">
+                {reservoirs.map((reservoir, index) => (
+                  <Stack.Item key={index} m={0.5}>
+                    <Button
+                      key={index}
+                      onClick={() => act('reservoir', { index })}
+                      width={7}
+                      tooltip={reservoir && "Eject"}>
+                      {
+                        reservoir
+                          ? <><Icon name="eject" /> {reservoir}</>
+                          : "None"
+                      }
+                    </Button>
+                  </Stack.Item>
+                ))}
+              </Stack>
+            </Section>
 
-        Memory:<br />
-        {buttons.map((button, index) => (
-          <>
-            M{index+1}
-            <Button
-              onClick={() => act('save', { index })}>
-              Save
-            </Button>
-            <Button
-              onClick={() => act('load', { index })}>
-              Load
-            </Button>
-            <Button
-              onClick={() => act('run', { index })}>
-              Run
-            </Button>
-            <br />
-          </>
-        ))}
-
-        <Input
-          value={inputValue}
-          onInput={(_event, value) => { act('updateInputValue', { value }); }}
-          height={7}
-          width={'100%'}
-          // The load button would break if we pressed it between the input's act and the next refresh.
-          // This ensures a refresh after every load button click
-          key={loadTimestamp}
-        />
-        <br />
-
-        inputValue: {inputValue} /
-        sx: {sx} /
-        tx: {tx} /
-        ax: {ax}
-
+            <Section title="Memory">
+              <em>
+                SX: {sx} -
+                AX: {tx} -
+                TX: {ax}
+              </em>
+              <Stack wrap>
+                {buttons.map((button, index) => (
+                  <Stack.Item key={index} style={{ border: "1px solid white", "border-radius": "0.2rem" }} p={1} m={1}>
+                    M{index+1}<br />
+                    <Button
+                      onClick={() => act('save', { index })}
+                      tooltip="Save"
+                      color="blue"
+                      icon="save" />
+                    <Button
+                      onClick={() => act('load', { index })}
+                      tooltip="Load"
+                      color="yellow"
+                      icon="download"
+                      disabled={!button} />
+                    <Button
+                      onClick={() => act('run', { index })}
+                      tooltip="Run"
+                      color="green"
+                      icon="play"
+                      disabled={!button} />
+                  </Stack.Item>
+                ))}
+              </Stack>
+            </Section>
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
