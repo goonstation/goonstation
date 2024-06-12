71f08e9c3c1b263e4b0976b5c2472d59d324af1d
Garash2k <6396368+Garash2k@users.noreply.github.com>
2024-06-12 11:57:12 -0400

    Revert "sans MapVoteReport"
    This reverts commit 04abfef444adb03db8ba77b51966d0cc24987bd9.

---
+++ tgui/packages/tgui/interfaces/MapVoteReport/index.tsx
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * @copyright 2024
+ * @author Garash2k (https://github.com/Garash2k)
+ * @license ISC
+ */
+
+import { Window } from '../../layouts';
+import { useBackend } from '../../backend';
+import { Box, Stack } from '../../components';
+import { MapPanel } from '../MapVote';
+import { MapVoteReportData } from './type';
+
+export const MapVoteReport = (_props, context) => {
+  const { data } = useBackend<MapVoteReportData>(context);
+  const { mapList, winner } = data;
+
+  return (
+    <Window height={185} width={(126 * mapList.length) + 6}>
+      <Window.Content>
+        <Stack>
+          {mapList.map(map => {
+            return (
+              <MapPanel key={map.name} mapName={map.name} mapThumbnail={map.thumbnail} winner={map.name === winner}>
+                <VoteCountLabel voteCount={map.count} />
+                {map.voters && <Voters voters={map.voters} />}
+              </MapPanel>
+            );
+          })}
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
+
+const VoteCountLabel = props => {
+  return (
+    <Box size={1.5} bold>
+      {props.voteCount || 0} vote{props.voteCount > 1 && "s"}
+    </Box>
+  );
+};
+
+const Voters = props => {
+  return (
+    <Box
+      scrollable
+      maxHeight={6}
+      style={{
+        'word-break': 'break-word',
+        'overflow': 'hidden',
+      }}
+      align="left"
+      fontSize={0.8}>
+      {props.voters.join(', ')}
+    </Box>
+  );
+};
