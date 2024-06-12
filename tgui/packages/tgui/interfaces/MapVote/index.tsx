dde6dbd009c758a2ef5e57d7f2f2adb0e2e7ea0a
Garash2k <6396368+Garash2k@users.noreply.github.com>
2024-06-12 11:26:17 -0400

    Revert "actual despair"
    This reverts commit bdec77017236bcaca179ba0249e5260912f2ad6c.

---
+++ tgui/packages/tgui/interfaces/MapVote/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * @copyright 2024
+ * @author Garash2k (https://github.com/Garash2k)
+ * @license ISC
+ */
+
+import { Window } from '../../layouts';
+
+export const MapVote = (_props, context) => {
+  return (
+    <Window>
+      <Window.Content />
+    </Window>
+  );
+};
+
---
+++ tgui/packages/tgui/interfaces/MapVoteReport/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @copyright 2024
+ * @author Garash2k (https://github.com/Garash2k)
+ * @license ISC
+ */
+
+import { Window } from '../../layouts';
+// import { MapPanel } from '../MapVote';
+
+export const MapVoteReport = (_props, context) => {
+
+  return (
+    <Window>
+      <Window.Content />
+    </Window>
+  );
+};
