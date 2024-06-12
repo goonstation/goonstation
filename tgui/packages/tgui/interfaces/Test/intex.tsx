6148f82145a7f908c9177e2f28171708191dd39b
Garash2k <6396368+Garash2k@users.noreply.github.com>
2024-06-12 12:08:07 -0400

    adding 2 interfaces in one branch

---
+++ tgui/packages/tgui/interfaces/Test/intex.tsx
@@ -0,0 +1,11 @@
+import { Window } from '../../layouts';
+
+export const Test = (_props, _context) => {
+  return (
+    <Window>
+      <Window.Content>
+        Test
+      </Window.Content>
+    </Window>
+  );
+};
---
+++ tgui/packages/tgui/interfaces/TestTwo/intex.tsx
@@ -0,0 +1,11 @@
+import { Window } from '../../layouts';
+
+export const TestTwo = (_props, _context) => {
+  return (
+    <Window>
+      <Window.Content>
+        Test
+      </Window.Content>
+    </Window>
+  );
+};
