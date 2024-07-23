5aad8ebcf5937a37e26a7718ee313faf964f6d76
ZeWaka <zewakagamer@gmail.com>
2024-07-23 00:25:16 -0700

    Port DataInput interface to TS & React

--- tgui/packages/tgui/interfaces/common/DataInput.tsx
+++ tgui/packages/tgui/interfaces/common/DataInput.tsx
@@ -3,17 +3,23 @@
  * SPDX-License-Identifier: MIT
  */
 
+import {
+  Button,
+  ColorBox,
+  Input,
+  LabeledList,
+  NumberInput,
+  Section,
+  Tooltip,
+} from 'tgui-core/components';
+
 import { useBackend } from '../../backend';
-import { Button, ColorBox, Input, LabeledList, NumberInput, Section, Tooltip } from '../../components';
 
-export const DataInputOptions = props => {
-  const {
-    options,
-  } = props;
+export const DataInputOptions = (props) => {
+  const { options } = props;
 
-  return (
-    options && Object.keys(options).length ? (
-      Object.keys(options).map((optionName, sectionIndex) => (
+  return options && Object.keys(options).length
+    ? Object.keys(options).map((optionName, sectionIndex) => (
         <DataInputEntry
           key={optionName}
           type={options[optionName].type}
@@ -26,14 +32,12 @@ export const DataInputOptions = props => {
           b={options[optionName].b}
         />
       ))
-    )
-      : ""
-  );
+    : '';
 };
 
-const DataInputEntry = (props, context) => {
+const DataInputEntry = (props) => {
   const { name, description, type } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
 
   const dataEntryTypes = {
     Number: <DataInputIntegerEntry {...props} />,
@@ -44,10 +48,10 @@ const DataInputEntry = (props, context) => {
     // File: <DataInputFileEntry {...props} />,
     // Direction: <DataInputDirectionEntry {...props} />,
     // JSON: <DataInputJSONEntry {...props} />,
-    "Ref": <DataInputRefEntry {...props} />,
-    "Mob Reference": <DataInputRefEntry {...props} />,
-    "Reference Picker": <DataInputRefEntry {...props} />,
-    "Bit Field": <DataInputBitFieldEntry {...props} />,
+    Ref: <DataInputRefEntry {...props} />,
+    'Mob Reference': <DataInputRefEntry {...props} />,
+    'Reference Picker': <DataInputRefEntry {...props} />,
+    'Bit Field': <DataInputBitFieldEntry {...props} />,
     // DATA_INPUT_TURF_BY_COORDS
     // DATA_INPUT_REFPICKER
     // DATA_INPUT_NEW_INSTANCE
@@ -58,57 +62,64 @@ const DataInputEntry = (props, context) => {
 
     // DATA_INPUT_LIST_BUILD
     // DATA_INPUT_LIST_EDIT
-    "Children of Type": <DataInputListEntry {...props} />,
-    "List Var": <DataInputListEntry {...props} />,
+    'Children of Type': <DataInputListEntry {...props} />,
+    'List Var': <DataInputListEntry {...props} />,
     List: <DataInputListEntry {...props} />,
   };
 
   return (
     <LabeledList.Item label={description}>
-      {dataEntryTypes[type] || act('unsupported_type', {
-        name: name,
-        type: type,
-      })}
+      {dataEntryTypes[type] ||
+        act('unsupported_type', {
+          name: name,
+          type: type,
+        })}
     </LabeledList.Item>
   );
 };
 
-export const DataInputBitFieldEntry = (props, context) => {
+export const DataInputBitFieldEntry = (props) => {
   const { value, tooltip, name, type, list } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
   return (
     <Section>
       {Array.apply(null, { length: 24 }).map((item, buttonIndex) => (
         <Button.Checkbox
           minWidth={4}
-          checked={value & (1 <<buttonIndex)}
+          checked={value & (1 << buttonIndex)}
           key={buttonIndex}
-          content={buttonIndex+1}
-          onClick={() => act('modify_value', {
-            name: name,
-            type: type,
-            value: value ^ (1 << buttonIndex),
-          })}
+          content={buttonIndex + 1}
+          onClick={() =>
+            act('modify_value', {
+              name: name,
+              type: type,
+              value: value ^ (1 << buttonIndex),
+            })
+          }
         />
       ))}
     </Section>
   );
 };
 
-const DataInputListEntry = (props, context) => {
+const DataInputListEntry = (props) => {
   const { value, tooltip, name, type, list } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
   return (
     <Section fill scrollable height={15}>
       {list.map((item, buttonIndex) => (
-        <Button fluid key={buttonIndex}
+        <Button
+          fluid
+          key={buttonIndex}
           selected={item === value}
           color="transparent"
-          onClick={() => act('modify_value', {
-            name: name,
-            value: item,
-            type: type,
-          })}
+          onClick={() =>
+            act('modify_value', {
+              name: name,
+              value: item,
+              type: type,
+            })
+          }
         >
           {item}
         </Button>
@@ -117,52 +128,59 @@ const DataInputListEntry = (props, context) => {
   );
 };
 
-const DataInputRefEntry = (props, context) => {
+const DataInputRefEntry = (props) => {
   const { value, tooltip, name, type } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
   return (
     <Tooltip position="bottom" content={tooltip}>
       <Button
         icon="location-crosshairs"
-        onClick={() => act('modify_ref_value', {
-          name: name,
-          type: type,
-        })}>
+        onClick={() =>
+          act('modify_ref_value', {
+            name: name,
+            type: type,
+          })
+        }
+      >
         {value}
       </Button>
     </Tooltip>
   );
 };
 
-const DataInputColorEntry = (props, context) => {
+const DataInputColorEntry = (props) => {
   const { value, tooltip, name, type } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
   return (
     <Tooltip position="bottom" content={tooltip}>
       <Button
         icon="pencil-alt"
-        onClick={() => act('modify_color_value', {
-          name: name,
-          type: type,
-        })} />
-      <ColorBox
-        color={value}
-        mr={0.5} />
+        onClick={() =>
+          act('modify_color_value', {
+            name: name,
+            type: type,
+          })
+        }
+      />
+      <ColorBox color={value} mr={0.5} />
       <Input
         value={value}
         width="90px"
-        onInput={(e, value) => act('modify_value', {
-          name: name,
-          value: value,
-          type: type,
-        })} />
+        onInput={(e, value) =>
+          act('modify_value', {
+            name: name,
+            value: value,
+            type: type,
+          })
+        }
+      />
     </Tooltip>
   );
 };
 
-const DataInputIntegerEntry = (props, context) => {
+const DataInputIntegerEntry = (props) => {
   const { value, tooltip, name, type, a, b } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
   return (
     <Tooltip position="bottom" content={tooltip}>
       <NumberInput
@@ -171,47 +189,56 @@ const DataInputIntegerEntry = (props, context) => {
         maxValue={b | 100}
         stepPixelSize={5}
         width="39px"
-        onDrag={(e, value) => act('modify_value', {
-          name: name,
-          value: value,
-          type: type,
-        })} />
-    </Tooltip>
-  );
-};
-
-const DataInputBoolEntry = (props, context) => {
-  const { value, tooltip, name, type } = props;
-  const { act } = useBackend(context);
-  return (
-    <Tooltip position="bottom" content={tooltip}>
-      <Button.Checkbox
-        checked={value}
-        // content={toggleOption}
-        onClick={() => act('modify_value', {
-          name: name,
-          value: !value,
-          type: type,
-        })}
+        step={1}
+        onDrag={(value) =>
+          act('modify_value', {
+            name: name,
+            value: value,
+            type: type,
+          })
+        }
       />
     </Tooltip>
   );
 };
 
-const DataInputTextEntry = (props, context) => {
+const DataInputBoolEntry = (props) => {
   const { value, tooltip, name, type } = props;
-  const { act } = useBackend(context);
+  const { act } = useBackend();
+  return (
+    <Tooltip position="bottom" content={tooltip}>
+      <Button.Checkbox
+        checked={value}
+        // content={toggleOption}
+        onClick={() =>
+          act('modify_value', {
+            name: name,
+            value: !value,
+            type: type,
+          })
+        }
+      />
+    </Tooltip>
+  );
+};
+
+const DataInputTextEntry = (props) => {
+  const { value, tooltip, name, type } = props;
+  const { act } = useBackend();
 
   return (
     <Tooltip position="bottom" content={tooltip}>
       <Input
         value={value}
         width="200px"
-        onInput={(e, value) => act('modify_value', {
-          name: name,
-          value: value,
-          type: type,
-        })} />
+        onInput={(e, value) =>
+          act('modify_value', {
+            name: name,
+            value: value,
+            type: type,
+          })
+        }
+      />
     </Tooltip>
   );
 };
