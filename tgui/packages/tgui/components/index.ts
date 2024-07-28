d5f6eaf79af9d9303dbb5fc7d54c8f5b7c303dce
ZeWaka <zewakagamer@gmail.com>
2024-07-27 22:48:58 -0700

    Nix RestrictedInput for tgui-core

--- tgui/packages/tgui/components/index.ts
+++ tgui/packages/tgui/components/index.ts
@@ -33,7 +33,6 @@ export { Modal } from './Modal';
 export { NoticeBox } from './NoticeBox';
 export { NumberInput } from './NumberInput';
 export { ProgressBar } from './ProgressBar';
-export { RestrictedInput } from './RestrictedInput';
 export { RoundGauge } from './RoundGauge';
 export { Section } from './Section';
 export { Slider } from './Slider';
