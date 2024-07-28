49288d5c6b89005d2d588641758ebf2c4b4ef741
ZeWaka <zewakagamer@gmail.com>
2024-07-28 01:02:13 -0700

    listinputmodal from upstream

--- tgui/packages/tgui/interfaces/common/InputButtons.tsx
+++ tgui/packages/tgui/interfaces/common/InputButtons.tsx
@@ -5,67 +5,99 @@
  * @license ISC
  */
 
-import { Box, Button, Stack } from 'tgui-core/components';
+import { Box, Button, Flex } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
 
-type InputButtonsData = {};
-
-type InputButtonsProps = {
-  input: string | number | null;
-  inputIsValid?: Validator;
+type InputButtonsData = {
+  large_buttons: boolean;
+  swapped_buttons: boolean;
 };
 
-export type Validator = {
-  isValid: boolean;
-  error: string | null;
+type InputButtonsProps = {
+  input: string | number | string[];
+  on_submit?: () => void;
+  on_cancel?: () => void;
+  message?: string;
 };
 
 export const InputButtons = (props: InputButtonsProps) => {
-  const { act } = useBackend<InputButtonsData>();
-  const { input, inputIsValid } = props;
+  const { act, data } = useBackend<InputButtonsData>();
+  const { large_buttons, swapped_buttons } = data;
+  const { input, message, on_submit, on_cancel } = props;
+
+  let on_submit_actual = on_submit;
+  if (!on_submit_actual) {
+    on_submit_actual = () => {
+      act('submit', { entry: input });
+    };
+  }
+
+  let on_cancel_actual = on_cancel;
+  if (!on_cancel_actual) {
+    on_cancel_actual = () => {
+      act('cancel');
+    };
+  }
 
   const submitButton = (
     <Button
       color="good"
-      disabled={inputIsValid && !inputIsValid.isValid}
-      fluid={false}
-      onClick={() => act('submit', { entry: input })}
-      pt={0}
+      fluid={!!large_buttons}
+      height={!!large_buttons && 2}
+      onClick={on_submit_actual}
+      m={0.5}
+      pl={2}
+      pr={2}
+      pt={large_buttons ? 0.33 : 0}
       textAlign="center"
-      tooltip={inputIsValid?.error || null}
-      width={6}
+      tooltip={large_buttons && message}
+      width={!large_buttons && 6}
     >
-      {'Submit'}
+      {large_buttons ? 'SUBMIT' : 'Submit'}
     </Button>
   );
   const cancelButton = (
     <Button
       color="bad"
-      fluid={false}
-      onClick={() => act('cancel')}
-      pt={0}
+      fluid={!!large_buttons}
+      height={!!large_buttons && 2}
+      onClick={on_cancel_actual}
+      m={0.5}
+      pl={2}
+      pr={2}
+      pt={large_buttons ? 0.33 : 0}
       textAlign="center"
-      width={6}
+      width={!large_buttons && 6}
     >
-      {'Cancel'}
+      {large_buttons ? 'CANCEL' : 'Cancel'}
     </Button>
   );
 
-  const leftButton = cancelButton;
-  const rightButton = submitButton;
-
   return (
-    <Stack>
-      <Stack.Item>{leftButton}</Stack.Item>
-      <Stack.Item grow>
-        {inputIsValid && !inputIsValid.isValid && inputIsValid.error && (
-          <Box color="average" nowrap textAlign="center">
-            {inputIsValid.error}
+    <Flex
+      align="center"
+      direction={!swapped_buttons ? 'row' : 'row-reverse'}
+      fill
+      justify="space-around"
+    >
+      {large_buttons ? (
+        <Flex.Item grow>{cancelButton}</Flex.Item>
+      ) : (
+        <Flex.Item>{cancelButton}</Flex.Item>
+      )}
+      {!large_buttons && message && (
+        <Flex.Item>
+          <Box color="label" textAlign="center">
+            {message}
           </Box>
-        )}
-      </Stack.Item>
-      <Stack.Item>{rightButton}</Stack.Item>
-    </Stack>
+        </Flex.Item>
+      )}
+      {large_buttons ? (
+        <Flex.Item grow>{submitButton}</Flex.Item>
+      ) : (
+        <Flex.Item>{submitButton}</Flex.Item>
+      )}
+    </Flex>
   );
 };
