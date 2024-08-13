023219d8d99f622322dc16a527ac906ed362293b
Garash2k <guillaumechabot1000@gmail.com>
2024-08-12 21:11:16 -0400

    chemicompiler textarea fix (#20284)

--- tgui/packages/tgui/interfaces/ChemiCompiler/ChemiCompilerTextArea.tsx
+++ tgui/packages/tgui/interfaces/ChemiCompiler/ChemiCompilerTextArea.tsx
@@ -5,6 +5,7 @@
  * @license ISC
  */
 
+import { useEffect, useState } from 'react';
 import { TextArea } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
@@ -13,14 +14,22 @@ import { ChemiCompilerData } from './type';
 export const ChemiCompilerTextArea = () => {
   const { act, data } = useBackend<ChemiCompilerData>();
   const { inputValue, loadTimestamp, theme } = data;
+
+  const [localInputValue, setLocalInputValue] = useState(inputValue);
+
+  // When loadTimestamp changes, it means a load button was clicked, so only then should we erase local input value with what was received from the server.
+  useEffect(() => {
+    setLocalInputValue(inputValue);
+  }, [loadTimestamp]);
+
   return (
     <TextArea
-      value={inputValue}
-      onInput={(_event, value) => act('updateInputValue', { value })}
+      value={localInputValue}
+      onInput={(_event, value) => {
+        setLocalInputValue(value);
+        act('updateInputValue', { value });
+      }}
       height="100%"
-      // The load button would break if we pressed it between the input's act and the next refresh.
-      // This ensures a refresh after every load button click
-      key={loadTimestamp}
       fontFamily="Consolas, monospace"
       fontSize="13px"
       style={{
