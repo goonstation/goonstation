c775a4e4800a75eb316064d0ffeab5fb81c8641b
Mordent <mordent.goonstation@gmail.com>
2024-08-19 18:51:43 -0400

    Implement BugReportForm interface in React

--- tgui/packages/tgui/interfaces/BugReportForm.tsx
+++ tgui/packages/tgui/interfaces/BugReportForm.tsx
@@ -1,160 +1,258 @@
 /**
  * @file
- * @copyright 2022 pali (https://github.com/pali6)
+ * @copyright 2022
+ * @author pali (https://github.com/pali6)
+ * @author Mordent (https://github.com/mordent-goonstation)
  * @license MIT
  */
 
-export const BugReportForm = () => {};
+import { ChangeEventHandler, useCallback, useState } from 'react';
+import { Flex, Input, Section } from 'tgui-core/components';
 
-// TODO-REACT unstub
-/*
-import { useBackend, useLocalState } from '../backend';
-import { Flex, Input, Section } from '../components';
+import { useBackend } from '../backend';
 import { ButtonCheckbox } from '../components/Button';
 import { Window } from '../layouts';
 
-export const Textarea = (props, context) => {
+type Tag = [string, string, string];
+
+const tags: Tag[] = [
+  [
+    'Trivial',
+    'TRIVIAL',
+    'A bug that is extremely trivial, such as a spelling issue.',
+  ],
+  [
+    'Minor',
+    'MINOR',
+    'A bug that does not impact usage of a feature. These are often visual issues.',
+  ],
+  [
+    'Major',
+    'MAJOR',
+    'A bug that significantly impacts the usage of a feature.',
+  ],
+  [
+    'Critical',
+    'CRITICAL',
+    'A bug that significantly impacts the progression of the round.',
+  ],
+];
+
+interface BugReportFormTextAreaProps {
+  onChange: (value: string) => void;
+  placeholder?: string;
+  value?: string;
+}
+
+export const BugReportFormTextArea = (props: BugReportFormTextAreaProps) => {
+  const { onChange, placeholder, value } = props;
+  const handleChange = useCallback<ChangeEventHandler<HTMLTextAreaElement>>(
+    (e) => onChange(e.target.value),
+    [onChange],
+  );
   return (
     <textarea
       rows={4}
       style={{
-        "overflow-y": "hidden",
-        "width": "100%",
-        "background-color": "black",
-        "border": "solid 1px #6992c2",
-        "color": "white",
+        overflowY: 'hidden',
+        width: '100%',
+        backgroundColor: 'black',
+        border: 'solid 1px #6992c2',
+        color: 'white',
       }}
-      onInput={(e) => {
-        e.target.style.height = "auto";
-        e.target.style.height = (e.target.scrollHeight) + "px";
-      }}
-      id={props.id}
-      placeholder={props.placeholder}
-    >
-      {props.defaultText}
-    </textarea>
+      // TODO-REACT Make this auto-size again
+      // onInput={(e) => {
+      //  e.target.style.height = 'auto';
+      //  e.target.style.height = e.target.scrollHeight + 'px';
+      // }}
+      onChange={handleChange}
+      placeholder={placeholder}
+      value={value || ''}
+    />
   );
 };
 
-export const InputTitle = (props, context) => {
+export const InputTitle = (props) => {
   return (
     <h2>
       {props.children}
-      {props.required && (<span style={{ "color": "red" }}>{" *"}</span>)}
+      {props.required && <span style={{ color: 'red' }}>{' *'}</span>}
     </h2>
   );
 };
 
-export const BugReportForm = (props, context) => {
-  const { act, data } = useBackend(context);
+interface FormState {
+  additional: string;
+  description: string;
+  expectedBehavior: string;
+  steps: string;
+  title: string;
+}
 
-  const [isSecret, setIsSecret] = useLocalState(context, 'is_secret', false);
-  const [chosenTag, setTag] = useLocalState(context, 'tag', 'MINOR');
+const initialFormState: FormState = {
+  additional: '',
+  description: '',
+  expectedBehavior: '',
+  steps: '',
+  title: '',
+};
 
-  const tags = [
-    ["Trivial", "TRIVIAL", "A bug that is extremely trivial, such as a spelling issue."],
-    ["Minor", "MINOR", "A bug that does not impact usage of a feature. These are often visual issues."],
-    ["Major", "MAJOR", "A bug that significantly impacts the usage of a feature."],
-    ["Critical", "CRITICAL", "A bug that significantly impacts the progression of the round."],
-  ];
+export const BugReportForm = () => {
+  const { act } = useBackend();
 
-  const submit = () => {
-    let data = {
+  const [isSecret, setIsSecret] = useState(false);
+  const [chosenTag, setTag] = useState('MINOR');
+  const [formState, setFormState] = useState(initialFormState);
+
+  const handleSubmit = useCallback(() => {
+    const submitData = {
       secret: isSecret,
       tags: [chosenTag],
-      steps: document.getElementById("steps").value,
-      additional: document.getElementById("additional").value,
-      title: document.getElementById("title").getElementsByTagName('input')[0].value,
-      description: document.getElementById("description").getElementsByTagName('input')[0].value,
-      expected_behavior: document.getElementById("expected_behavior").getElementsByTagName('input')[0].value,
+      steps: formState.steps,
+      additional: formState.additional,
+      title: formState.title,
+      description: formState.description,
+      expected_behavior: formState.expectedBehavior,
     };
-    if (!data.title || !data.description || !data.expected_behavior || !data.steps) {
-      alert("Please fill out all required fields!");
+    if (
+      !submitData.title ||
+      !submitData.description ||
+      !submitData.expected_behavior ||
+      !submitData.steps
+    ) {
+      alert('Please fill out all required fields!');
       return;
     }
-    act("confirm", data);
-  };
-
+    act('confirm', submitData);
+  }, [act, isSecret, chosenTag, formState]);
+  const handleCancel = useCallback(() => act('cancel'), [act]);
+  // TODO: reduce copy-paste
+  const handleTitleChange = useCallback(
+    (_e: unknown, value: string) =>
+      setFormState((prevState) => ({ ...prevState, title: value })),
+    [],
+  );
+  const handleDescriptionChange = useCallback(
+    (_e: unknown, value: string) =>
+      setFormState((prevState) => ({ ...prevState, description: value })),
+    [],
+  );
+  const handleStepsChange = useCallback(
+    (value: string) =>
+      setFormState((prevState) => ({ ...prevState, steps: value })),
+    [],
+  );
+  const handleExpectedBehaviorChange = useCallback(
+    (_e: unknown, value: string) =>
+      setFormState((prevState) => ({ ...prevState, expectedBehavior: value })),
+    [],
+  );
+  const handleAdditionalChange = useCallback(
+    (value: string) =>
+      setFormState((prevState) => ({ ...prevState, additional: value })),
+    [],
+  );
   return (
-    <Window
-      title={"Bug Report Form"}
-      width={600}
-      height={700}>
+    <Window title="Bug Report Form" width={600} height={700}>
       <Window.Content>
         <Section fill scrollable>
           <Flex direction="column" height="100%">
-            <Flex.Item style={{ "text-align": "center" }}>
+            <Flex.Item style={{ 'text-align': 'center' }}>
               <a
                 href="https://github.com/goonstation/goonstation/issues/new?assignees=&labels=&template=bug_report.yml"
-                target="_blank" rel="noreferrer"
+                target="_blank"
+                rel="noreferrer"
                 style={{
-                  "color": "#6992c2",
+                  color: '#6992c2',
                 }}
               >
                 If you have a GitHub account click here instead
               </a>
             </Flex.Item>
             <Flex.Item>
-              <InputTitle required>{"Title"}</InputTitle>
-              <Input width="100%" id="title" />
+              <InputTitle required>Title</InputTitle>
+              <Input
+                width="100%"
+                value={formState.title}
+                onChange={handleTitleChange}
+              />
             </Flex.Item>
             <Flex.Item my={2}>
-              <h2>{"Tags"}</h2>
-              {tags.map(tag =>
-                (
-                  <ButtonCheckbox
-                    key={tag[1]}
-                    checked={tag[1] === chosenTag}
-                    onClick={() => setTag(tag[1])}
-                    tooltip={tag[2]}
-                    tooltipPosition="bottom">
-                    {tag[0]}
-                  </ButtonCheckbox>
-                )
-              )}
+              <h2>Tags</h2>
+              {tags.map((tag) => (
+                <ButtonCheckbox
+                  key={tag[1]}
+                  checked={tag[1] === chosenTag}
+                  onClick={() => setTag(tag[1])}
+                  tooltip={tag[2]}
+                  tooltipPosition="bottom"
+                >
+                  {tag[0]}
+                </ButtonCheckbox>
+              ))}
             </Flex.Item>
             <Flex.Item my={2}>
-              <InputTitle required>{"Description"}</InputTitle>
-              {"Give a short description of the bug"}
-              <Input width="100%" id="description" />
+              <InputTitle required>Description</InputTitle>
+              Give a short description of the bug
+              <Input
+                width="100%"
+                value={formState.description}
+                onChange={handleDescriptionChange}
+              />
             </Flex.Item>
             <Flex.Item my={2}>
-              <InputTitle required>{"Steps To Reproduce"}</InputTitle>
-              {"Give a list of steps to reproduce this issue"}
-              <Textarea id="steps" placeholder="1.\n2.\n3." />
+              <InputTitle required>Steps To Reproduce</InputTitle>
+              Give a list of steps to reproduce this issue
+              <BugReportFormTextArea
+                onChange={handleStepsChange}
+                value={formState.steps}
+                placeholder="1.\n2.\n3."
+              />
             </Flex.Item>
             <Flex.Item my={2}>
-              <InputTitle required>{"Expected Behavior"}</InputTitle>
-              {"Give a short description of what you expected to happen"}
-              <Input width="100%" id="expected_behavior" />
+              <InputTitle required>Expected Behavior</InputTitle>
+              Give a short description of what you expected to happen
+              <Input
+                width="100%"
+                value={formState.expectedBehavior}
+                onChange={handleExpectedBehaviorChange}
+              />
             </Flex.Item>
             <Flex.Item my={2}>
-              <h2>{"Additional Information & Screenshots"}</h2>
-              {"Add screenshots and any other information here"}
-              <Textarea id="additional" />
+              <h2>Additional Information & Screenshots</h2>
+              Add screenshots and any other information here
+              <BugReportFormTextArea
+                value={formState.additional}
+                onChange={handleAdditionalChange}
+              />
             </Flex.Item>
             <Flex.Item my={2}>
-              <h2>{"Is this bug an exploit or related to secret content?"}</h2>
-              <ButtonCheckbox checked={isSecret} onClick={() => { setIsSecret(!isSecret); }}>
-                {"Exploit / Secret"}
+              <h2>Is this bug an exploit or related to secret content?</h2>
+              <ButtonCheckbox
+                checked={isSecret}
+                onClick={() => {
+                  setIsSecret(!isSecret);
+                }}
+              >
+                Exploit / Secret
               </ButtonCheckbox>
             </Flex.Item>
             <Flex.Item my={2}>
-              <Flex style={{ "justify-content": "center" }}>
+              <Flex style={{ 'justify-content': 'center' }}>
                 <Flex.Item mx={1}>
                   <div
                     className="Button Button--color--default"
-                    onClick={submit}
+                    onClick={handleSubmit}
                   >
-                    {"Submit"}
+                    Submit
                   </div>
                 </Flex.Item>
                 <Flex.Item mx={1}>
                   <div
                     className="Button Button--color--default"
-                    onClick={() => act("cancel")}
+                    onClick={handleCancel}
                   >
-                    {"Cancel"}
+                    Cancel
                   </div>
                 </Flex.Item>
               </Flex>
@@ -165,4 +263,3 @@ export const BugReportForm = (props, context) => {
     </Window>
   );
 };
-*/
