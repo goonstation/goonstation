13ac049cf394b5085a2450c3fecae39c6c36ed74
FlameArrow57 <53062374+FlameArrow57@users.noreply.github.com>
2025-01-25 09:13:06 -0800

    improve UI

--- tgui/packages/tgui/interfaces/Oven.tsx
+++ tgui/packages/tgui/interfaces/Oven.tsx
@@ -7,6 +7,7 @@
 
 import {
   Button,
+  Divider,
   Icon,
   LabeledList,
   Modal,
@@ -46,7 +47,7 @@ export const Oven = () => {
   } = data;
 
   return (
-    <Window title="Cookomatic Multi-Oven" width={380} height={630}>
+    <Window title="Cookomatic Multi-Oven" width={420} height={555}>
       <Window.Content>
         <Section
           title="Settings"
@@ -59,88 +60,42 @@ export const Oven = () => {
             </Button>
           }
         >
-          <Stack vertical align="center" fontSize={1.25}>
-            <Stack.Item fontSize={1.5}>
-              <Button
-                color={time === 1 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 1 })}
-              >
-                1
-              </Button>
-              <Button
-                color={time === 2 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 2 })}
-              >
-                2
-              </Button>
-              <Button
-                color={time === 3 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 3 })}
-              >
-                3
-              </Button>
-              <Button
-                color={time === 4 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 4 })}
-              >
-                4
-              </Button>
-              <Button
-                color={time === 5 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 5 })}
-              >
-                5
-              </Button>
+          <Stack fontSize={1.5} justify="center">
+            <Stack.Item vertical>
+              <Stack.Item>
+                <OvenDialbuttons min={1} max={5} time={time} />
+              </Stack.Item>
+              <Stack.Item>
+                <OvenDialbuttons min={6} max={10} time={time} />
+              </Stack.Item>
             </Stack.Item>
-            <Stack.Item mt={0.5} fontSize={1.5}>
-              <Button
-                color={time === 6 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 6 })}
-              >
-                6
-              </Button>
-              <Button
-                color={time === 7 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 7 })}
-              >
-                7
-              </Button>
-              <Button
-                color={time === 8 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 8 })}
-              >
-                8
-              </Button>
-              <Button
-                color={time === 9 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 9 })}
-              >
-                9
-              </Button>
-              <Button
-                color={time === 10 ? 'orange' : 'grey'}
-                onClick={() => act('set_time', { time: 10 })}
-              >
-                10
-              </Button>
+            <Stack.Item vertical>
+              <Stack.Item>
+                <Button
+                  color={heat === 'High' ? 'red' : 'grey'}
+                  onClick={() => act('set_heat', { heat: 'High' })}
+                  minWidth="75px"
+                  textAlign="center"
+                >
+                  High
+                </Button>
+              </Stack.Item>
+              <Stack.Item mt={0.5}>
+                <Button
+                  color={heat === 'Low' ? 'blue' : 'grey'}
+                  onClick={() => act('set_heat', { heat: 'Low' })}
+                  minWidth="75px"
+                  textAlign="center"
+                >
+                  Low
+                </Button>
+              </Stack.Item>
             </Stack.Item>
+          </Stack>
+          <Stack justify="center">
             <Stack.Item>
-              <Button
-                color={heat === 'High' ? 'orange' : 'grey'}
-                onClick={() => act('set_heat', { heat: 'High' })}
-              >
-                High
-              </Button>
-              <Button
-                color={heat === 'Low' ? 'orange' : 'grey'}
-                onClick={() => act('set_heat', { heat: 'Low' })}
-              >
-                Low
-              </Button>
-            </Stack.Item>
-            <Stack.Item mt={0.5}>
-              <Button selected onClick={() => act('start')}>
-                Start!
+              <Button selected onClick={() => act('start')} fontSize={2} mt={0.5}>
+                Cook
               </Button>
             </Stack.Item>
           </Stack>
@@ -155,61 +110,84 @@ export const Oven = () => {
           }
         >
           {content_icons && content_icons.length ? (
-            <Stack vertical>
+            <Stack vertical minHeight="110px" maxHeight="110px">
               {content_icons.map((item, index) => (
-                <Stack.Item key={index}>
-                  <img
-                    height={48}
-                    width={48}
-                    src={`data:image/png;base64,${item}`}
-                  />
-                  {`     ${content_names[index]}`}
-                  <Button
-                    onClick={() => act('eject', { ejected_item: index + 1 })}
-                    ml={3}
-                  >
-                    <Icon name="eject" />
-                  </Button>
-                </Stack.Item>
+                <Stack key={index} style={{ borderBottom: '0.5px #555 solid' }}>
+                  <Stack.Item grow style={{
+                          display: 'flex',
+                        }}>
+                    <img
+                      height={26}
+                      width={26}
+                      src={`data:image/png;base64,${item}`}
+                      style={{ transform: 'translate(0, -4px)' }}
+                    />
+                    {content_names[index]}
+                  </Stack.Item>
+                  <Stack.Item>
+                    <Button
+                      onClick={() => act('eject', { ejected_item: index + 1 })}
+                      ml={3}
+                      style={{ transform: 'translate(0, 4px)' }}
+                    >
+                      <Icon name="eject" />
+                    </Button>
+                  </Stack.Item>
+                </Stack>
               ))}
             </Stack>
           ) : (
-            '(Empty)'
+            <Stack minHeight="110px" maxHeight="110px"><Stack.Item>(Empty)</Stack.Item></Stack>
           )}
         </Section>
         {output_icon && (
-          <Section title="Potential Recipe">
-            <Stack>
-              {recipe_icons.map((item, index) => (
-                <Stack.Item key={index}>
-                  <img
-                    height={48}
-                    width={48}
-                    src={`data:image/png;base64,${item}`}
-                  />
-                  {recipe_names[index]}
-                </Stack.Item>
-              ))}
+          <Section grow>
+            <Stack align>
+              <Stack.Item grow>
+                <Section title="Potential Recipe">
+                  <Stack vertical>
+                    <Stack.Item>
+                    {recipe_icons.map((item, index) => (
+                      <Stack.Item key={index} style={{ borderBottom: '0.5px #555 solid', display: "flex" }}>
+                        <img
+                          height={26}
+                          width={26}
+                          src={`data:image/png;base64,${item}`}
+                          style={{ transform: 'translate(0, -4px)' }}
+                        />
+                        {recipe_names[index]}
+                      </Stack.Item>
+                    ))}
+                    </Stack.Item>
+                    <Stack.Item>
+                      <LabeledList>
+                        <LabeledList.Item label="Cook time">
+                          {cook_time}
+                        </LabeledList.Item>
+                      </LabeledList>
+                    </Stack.Item>
+                  </Stack>
+                </Section>
+              </Stack.Item>
+              <Stack.Item><Divider vertical /></Stack.Item>
+              <Stack.Item grow>
+                <Section title="Result">
+                  <Stack vertical align="center" textAlign="center">
+                    <Stack.Item>
+                      <img
+                        height={48}
+                        width={48}
+                        src={`data:image/png;base64,${output_icon}`}
+                        style={{ transform: 'translate(0, -4px)' }}
+                      />
+                    </Stack.Item>
+                    <Stack.Item>
+                     {output_name}
+                    </Stack.Item>
+                  </Stack>
+                </Section>
+              </Stack.Item>
             </Stack>
-            <Section title="Result">
-              <Stack vertical>
-                <Stack.Item>
-                  <img
-                    height={48}
-                    width={48}
-                    src={`data:image/png;base64,${output_icon}`}
-                  />
-                  {output_name}
-                </Stack.Item>
-                <Stack.Item>
-                  <LabeledList>
-                    <LabeledList.Item label="Cook time">
-                      {cook_time}
-                    </LabeledList.Item>
-                  </LabeledList>
-                </Stack.Item>
-              </Stack>
-            </Section>
           </Section>
         )}
         {!!cooking && (
@@ -220,4 +198,21 @@ export const Oven = () => {
       </Window.Content>
     </Window>
   );
+};
+
+const OvenDialbuttons = (props) => {
+  const { act } = useBackend();
+  const { min, max, time } = props;
+  const nodes: JSX.Element[] = [];
+  for (let i = min; i <= max; i++) {
+    const node = (
+      <Button key={i}
+        color={time === i ? 'default' : 'grey'}
+        onClick={() => act('set_time', { "time": i })}
+        width="40px"
+        textAlign="center">{i}
+      </Button>);
+    nodes.push(node);
+  }
+  return nodes;
 };
