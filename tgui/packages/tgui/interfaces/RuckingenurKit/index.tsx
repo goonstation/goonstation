5b10d6029722705008f95360d4c51c6fd88221b3
Garash2k <6396368+Garash2k@users.noreply.github.com>
2025-02-13 19:32:41 -0500

    filter feature

--- tgui/packages/tgui/interfaces/RuckingenurKit/index.tsx
+++ tgui/packages/tgui/interfaces/RuckingenurKit/index.tsx
@@ -5,7 +5,8 @@
  * @license MIT
  */
 
-import { Section } from 'tgui-core/components';
+import { useState } from 'react';
+import { Input, Section } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
 import { Window } from '../../layouts';
@@ -17,25 +18,42 @@ export const RuckingenurKit = () => {
   const { data } = useBackend<RuckingenurKitData>();
   const { scanned_items, hide_allowed, legacyElectronicFrameMode } = data;
 
+  const [filter, setFilter] = useState('');
+  const filterExp = new RegExp(filter, 'i');
+
   return (
     <Window width={925} height={420}>
       <Window.Content>
-        <Section title="Scanned Items" scrollable fill>
-          {scanned_items.map((scanned_item) =>
-            !legacyElectronicFrameMode ? (
-              <ScannedItem
-                ScannedItem={scanned_item}
-                key={scanned_item.ref}
-                hide_allowed={hide_allowed}
-              />
-            ) : (
-              <ScannedItemLegacyElectronicFrameMode
-                ScannedItem={scanned_item}
-                key={scanned_item.ref}
-                hide_allowed={hide_allowed}
-              />
-            ),
-          )}
+        <Section
+          title="Scanned Items"
+          scrollable
+          fill
+          buttons={
+            <label>
+              Filter items:{' '}
+              <Input value={filter} onInput={(_e, value) => setFilter(value)} />
+            </label>
+          }
+        >
+          {scanned_items
+            .filter(
+              (scanned_item) => !filter || scanned_item.name.match(filterExp),
+            )
+            .map((scanned_item) =>
+              !legacyElectronicFrameMode ? (
+                <ScannedItem
+                  ScannedItem={scanned_item}
+                  key={scanned_item.ref}
+                  hide_allowed={hide_allowed}
+                />
+              ) : (
+                <ScannedItemLegacyElectronicFrameMode
+                  ScannedItem={scanned_item}
+                  key={scanned_item.ref}
+                  hide_allowed={hide_allowed}
+                />
+              ),
+            )}
         </Section>
       </Window.Content>
     </Window>
