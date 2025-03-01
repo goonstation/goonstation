7ac62e8ad6d4b6612b5ecacd4855c95d1c078ea9
Mordent <mordent.goonstation@gmail.com>
2025-02-28 22:11:23 -0500

    Some GPS tidy up, make distress blink use CSS animation

--- tgui/packages/tgui/interfaces/GPS.tsx
+++ tgui/packages/tgui/interfaces/GPS.tsx
@@ -2,13 +2,15 @@
  * @file
  * @copyright 2025
  * @author FlameArrow57 (https://github.com/FlameArrow57)
+ * @author Mordent (https://github.com/mordent-goonstation)
  * @license ISC
  */
-import type { BooleanLike } from 'common/react';
-import React from 'react';
+import { type BooleanLike } from 'common/react';
+import { Fragment, memo, useCallback } from 'react';
 import {
   Button,
   Collapsible,
+  Icon,
   LabeledList,
   NumberInput,
   Section,
@@ -61,93 +63,99 @@ export const GPS = () => {
   } = data;
 
   return (
-    <Window title="GPS" width={460} height={610} theme="ntos">
+    <Window title="GPS" width={490} height={610} theme="ntos">
       <Window.Content>
-        <Section title={`GPS Device ${src_name}`}>
-          <Stack>
-            <Stack.Item>
-              <LabeledList>
-                <LabeledList.Item label="Coordinates">
-                  {src_x}, {src_y}
-                </LabeledList.Item>
-                <LabeledList.Item label="Identifier">
-                  <Button
-                    onClick={() => act('change_identifier')}
-                    tooltip={gpsTooltip}
-                  >
-                    {src_name.slice(5, src_name.length)}
-                  </Button>
-                </LabeledList.Item>
-                <LabeledList.Item label="Coords">
-                  x:{' '}
-                  <NumberInput
-                    value={track_x}
-                    width="3"
-                    minValue={1}
-                    maxValue={300}
-                    step={1}
-                    onChange={(x_val: number) => act('set_x', { x: x_val })}
-                  />
-                  y:{' '}
-                  <NumberInput
-                    value={track_y}
-                    width="3"
-                    minValue={1}
-                    maxValue={300}
-                    step={1}
-                    onChange={(y_val: number) => act('set_y', { y: y_val })}
-                  />
-                  <Button
-                    mt={0.5}
-                    onClick={() =>
-                      act('track_coords', { x: track_x, y: track_y })
-                    }
-                  >
-                    Track
-                  </Button>
-                </LabeledList.Item>
-              </LabeledList>
-            </Stack.Item>
-            <Stack.Item>
-              <LabeledList>
-                <LabeledList.Item label="Trackable">
-                  <Button.Checkbox
-                    onClick={() => act('toggle_trackable')}
-                    selected={trackable}
-                    checked={trackable}
-                  >
-                    {trackable ? 'Yes' : 'No'}
-                  </Button.Checkbox>
-                </LabeledList.Item>
-                <LabeledList.Item label="Send distress">
-                  <Button.Checkbox
-                    onClick={() => act('toggle_distress')}
-                    selected={distress}
-                    checked={distress}
-                  >
-                    {distress ? 'Yes' : 'No'}
-                  </Button.Checkbox>
-                </LabeledList.Item>
-                <LabeledList.Item label="Tracking">
-                  <Button onClick={() => act('track_gps')}>
-                    {tracking || 'None'}
-                  </Button>
-                </LabeledList.Item>
-              </LabeledList>
-            </Stack.Item>
-          </Stack>
-        </Section>
-        <Section title="Tracking">
-          <Collapsible title="GPS Devices">
-            <TrackableList gps_info={gps_info} />
-          </Collapsible>
-          <Collapsible title="Implants">
-            <TrackableList gps_info={imp_info} />
-          </Collapsible>
-          <Collapsible title="Warp Beacons">
-            <TrackableList gps_info={warp_info} />
-          </Collapsible>
-        </Section>
+        <Stack vertical fill>
+          <Stack.Item>
+            <Section title={`GPS Device ${src_name}`}>
+              <Stack>
+                <Stack.Item>
+                  <LabeledList>
+                    <LabeledList.Item label="Coordinates">
+                      {src_x}, {src_y}
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Identifier">
+                      <Button
+                        onClick={() => act('change_identifier')}
+                        tooltip={gpsTooltip}
+                      >
+                        {src_name.slice(5, src_name.length)}
+                      </Button>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Coords">
+                      x:{' '}
+                      <NumberInput
+                        value={track_x}
+                        width="3"
+                        minValue={1}
+                        maxValue={300}
+                        step={1}
+                        onChange={(x_val: number) => act('set_x', { x: x_val })}
+                      />
+                      y:{' '}
+                      <NumberInput
+                        value={track_y}
+                        width="3"
+                        minValue={1}
+                        maxValue={300}
+                        step={1}
+                        onChange={(y_val: number) => act('set_y', { y: y_val })}
+                      />
+                      <Button
+                        mt={0.5}
+                        onClick={() =>
+                          act('track_coords', { x: track_x, y: track_y })
+                        }
+                      >
+                        Track
+                      </Button>
+                    </LabeledList.Item>
+                  </LabeledList>
+                </Stack.Item>
+                <Stack.Item>
+                  <LabeledList>
+                    <LabeledList.Item label="Trackable">
+                      <Button.Checkbox
+                        onClick={() => act('toggle_trackable')}
+                        selected={trackable}
+                        checked={trackable}
+                      >
+                        {trackable ? 'Yes' : 'No'}
+                      </Button.Checkbox>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Send distress">
+                      <Button.Checkbox
+                        onClick={() => act('toggle_distress')}
+                        selected={distress}
+                        checked={distress}
+                      >
+                        {distress ? 'Yes' : 'No'}
+                      </Button.Checkbox>
+                    </LabeledList.Item>
+                    <LabeledList.Item label="Tracking">
+                      <Button onClick={() => act('track_gps')}>
+                        {tracking || 'None'}
+                      </Button>
+                    </LabeledList.Item>
+                  </LabeledList>
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+          <Stack.Item grow>
+            <Section title="Tracking" scrollable fill>
+              <Collapsible title="GPS Devices">
+                <TrackableList gps_info={gps_info} />
+              </Collapsible>
+              <Collapsible title="Implants">
+                <TrackableList gps_info={imp_info} />
+              </Collapsible>
+              <Collapsible title="Warp Beacons">
+                <TrackableList gps_info={warp_info} />
+              </Collapsible>
+            </Section>
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
@@ -157,47 +165,64 @@ interface TrackableListProps {
   gps_info: GPSTrackableData[];
 }
 
-let distress_red = false;
 const TrackableList = (props: TrackableListProps) => {
   const { act } = useBackend();
   const { gps_info } = props;
-
-  distress_red = !distress_red;
-
+  const handleTrack = useCallback(
+    (gps_ref: string) => act('track_gps', { gps_ref }),
+    [act],
+  );
   return gps_info.length ? (
     <Stack vertical>
       {gps_info.map((item) => (
-        <React.Fragment key={item.obj_ref}>
-          <Stack.Item>
-            <Stack>
-              <Stack.Item grow>
-                <Stack
-                  vertical
-                  textColor={item.distress && distress_red ? 'bad' : undefined}
-                >
-                  <Stack.Item>
-                    <strong>{item.name}</strong>
-                    {!!item.distress && ' [Distress!]'}
-                  </Stack.Item>
-                  <Stack.Item>
-                    <em>{`Located at (${item.x}), (${item.y}) | ${item.z_info}`}</em>
-                  </Stack.Item>
-                </Stack>
-              </Stack.Item>
-              <Stack.Item align="center">
-                <Button
-                  onClick={() => act('track_gps', { gps_ref: item.obj_ref })}
-                >
-                  Track
-                </Button>
-              </Stack.Item>
-            </Stack>
-          </Stack.Item>
+        <Fragment key={item.obj_ref}>
+          <TrackableItem {...item} onTrack={handleTrack} />
           <Stack.Divider />
-        </React.Fragment>
+        </Fragment>
       ))}
     </Stack>
   ) : (
     'None found'
   );
 };
+
+type TrackableItemProps = GPSTrackableData & {
+  onTrack: (gpsRef: string) => void;
+};
+
+const TrackableItem = memo((props: TrackableItemProps) => {
+  const { distress, name, obj_ref, onTrack, x, y, z_info } = props;
+  const handleTrackClick = useCallback(
+    () => onTrack(obj_ref),
+    [onTrack, obj_ref],
+  );
+  return (
+    <Stack.Item>
+      <Stack>
+        <Stack.Item grow>
+          <Stack vertical>
+            <Stack.Item>
+              <Stack>
+                <Stack.Item bold>{name}</Stack.Item>
+                {!!distress && (
+                  <Stack.Item
+                    className={distress ? 'color-orange--blinking' : undefined}
+                  >
+                    <Icon mr={1} name="exclamation-triangle" />
+                    In Distress
+                  </Stack.Item>
+                )}
+              </Stack>
+            </Stack.Item>
+            <Stack.Item>
+              <em>{`Located at (${x}, ${y}) | ${z_info}`}</em>
+            </Stack.Item>
+          </Stack>
+        </Stack.Item>
+        <Stack.Item align="center">
+          <Button onClick={handleTrackClick}>Track</Button>
+        </Stack.Item>
+      </Stack>
+    </Stack.Item>
+  );
+});
