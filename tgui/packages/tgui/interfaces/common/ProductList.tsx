098eeb63a663a6821951f91e33463b0e07339fa1
Mordent <mordent.goonstation@gmail.com>
2025-03-01 00:25:29 -0500

    Zebra GlassRecycler

---
+++ tgui/packages/tgui/interfaces/common/ProductList.tsx
@@ -0,0 +1,36 @@
+import { PropsWithChildren } from 'react';
+import { Button, Image, Stack } from 'tgui-core/components';
+
+type ProductListProps = PropsWithChildren<{}>;
+
+export const ProductList = (props: ProductListProps) => {
+  const { children } = props;
+  return children;
+};
+
+type ProductListItemProps = PropsWithChildren<{
+  canBuy: boolean;
+  cost?: React.ReactNode;
+  image?: string;
+  onBuy: () => void;
+}>;
+
+const ProductListItem = (props: ProductListItemProps) => {
+  const { canBuy, children, cost, image, onBuy } = props;
+  return (
+    <Stack align="center" className="candystripe" px={1}>
+      {image && (
+        <Stack.Item>
+          <Image src={`data:image/png;base64,${image}`} />
+        </Stack.Item>
+      )}
+      <Stack.Item grow>{children}</Stack.Item>
+      {cost !== null && cost !== undefined && <Stack.Item>{cost}</Stack.Item>}
+      <Stack.Item>
+        <Button onClick={onBuy} disabled={!canBuy} icon="plus" />
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+ProductList.Item = ProductListItem;
