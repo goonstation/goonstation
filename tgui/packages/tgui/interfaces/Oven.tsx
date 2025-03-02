b917871fd73a917c1138c078c7d39b03c5ca26b4
Mordent <mordent.goonstation@gmail.com>
2025-03-01 19:39:26 -0500

    Oven UI improvement

--- tgui/packages/tgui/interfaces/Oven.tsx
+++ tgui/packages/tgui/interfaces/Oven.tsx
@@ -16,14 +16,15 @@ import {
   Section,
   Stack,
 } from 'tgui-core/components';
+import { BooleanLike } from 'tgui-core/react';
 
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
 
-interface PacketInfo {
+interface OvenData {
   time: number;
   heat: string;
-  cooking: boolean;
+  cooking: BooleanLike;
   content_icons: string[];
   content_names: string[];
   recipe_icons: string[];
@@ -34,7 +35,7 @@ interface PacketInfo {
 }
 
 export const Oven = () => {
-  const { act, data } = useBackend<PacketInfo>();
+  const { act, data } = useBackend<OvenData>();
   const {
     time,
     heat,
@@ -51,159 +52,167 @@ export const Oven = () => {
   return (
     <Window title="Cookomatic Multi-Oven" width={420} height={600}>
       <Window.Content>
-        <Section
-          title="Settings"
-          buttons={
-            <Button
-              onClick={() => act('open_recipe_book')}
-              tooltip={'Warning: Slow'}
+        <Stack vertical fill>
+          <Stack.Item>
+            <Section
+              title="Settings"
+              buttons={
+                <Button
+                  onClick={() => act('open_recipe_book')}
+                  tooltip={'Warning: Slow'}
+                >
+                  Open Recipe Book
+                </Button>
+              }
             >
-              Open Recipe Book
-            </Button>
-          }
-        >
-          <Stack justify="center">
-            <Stack.Item>
-              <OvenDialbuttons min={1} max={5} time={time} />
-              <OvenDialbuttons min={6} max={10} time={time} />
-            </Stack.Item>
-            <Stack.Item>
-              <Stack.Item>
-                <Button
-                  selected={heat === 'High'}
-                  onClick={() => act('set_heat', { heat: 'High' })}
-                  minWidth="75px"
-                  textAlign="center"
-                >
-                  High
-                </Button>
-              </Stack.Item>
-              <Stack.Item mt={0.5}>
-                <Button
-                  selected={heat === 'Low'}
-                  onClick={() => act('set_heat', { heat: 'Low' })}
-                  minWidth="75px"
-                  textAlign="center"
-                >
-                  Low
-                </Button>
-              </Stack.Item>
-            </Stack.Item>
-          </Stack>
-          <Stack justify="center">
-            <Stack.Item>
-              <Button onClick={() => act('start')} fontSize={2} mt={0.5}>
-                Cook
-              </Button>
-            </Stack.Item>
-          </Stack>
-        </Section>
-        <Section
-          title="Contents"
-          scrollable={!!(content_icons && content_icons.length)}
-          buttons={
-            <Button onClick={() => act('eject_all')}>
-              <Icon name="eject" />
-            </Button>
-          }
-        >
-          {content_icons && content_icons.length ? (
-            <Stack vertical minHeight="130px" maxHeight="130px">
-              {content_icons.map((item, index) => (
-                <Stack key={index} style={{ borderBottom: '0.5px #555 solid' }}>
-                  <Stack.Item
-                    grow
-                    style={{
-                      display: 'flex',
-                    }}
-                  >
-                    <Image
-                      height="32px"
-                      width="32px"
-                      src={`data:image/png;base64,${item}`}
-                      style={{ transform: 'translate(0, -2px)' }}
-                    />
-                    <Box mt={1}>{content_names[index]}</Box>
-                  </Stack.Item>
+              <Stack justify="center">
+                <Stack.Item>
+                  <OvenDialbuttons min={1} max={5} time={time} />
+                  <OvenDialbuttons min={6} max={10} time={time} />
+                </Stack.Item>
+                <Stack.Item>
                   <Stack.Item>
                     <Button
-                      onClick={() => act('eject', { ejected_item: index + 1 })}
-                      ml={3}
-                      style={{ transform: 'translate(0, 4px)' }}
+                      selected={heat === 'High'}
+                      onClick={() => act('set_heat', { heat: 'High' })}
+                      minWidth="75px"
+                      textAlign="center"
                     >
-                      <Icon name="eject" />
+                      High
                     </Button>
                   </Stack.Item>
-                </Stack>
-              ))}
-            </Stack>
-          ) : (
-            <Stack minHeight="130px" maxHeight="130px">
-              <Stack.Item>(Empty)</Stack.Item>
-            </Stack>
-          )}
-        </Section>
-        <Section>
-          <Stack align>
-            <Stack.Item grow>
-              <Section title="Potential Recipe">
-                {output_icon ? (
-                  <Stack vertical>
-                    <Stack.Item>
-                      {recipe_icons.map((item, index) => (
-                        <Stack.Item
-                          key={index}
-                          style={{
-                            borderBottom: '0.5px #555 solid',
-                            display: 'flex',
-                          }}
+                  <Stack.Item mt={0.5}>
+                    <Button
+                      selected={heat === 'Low'}
+                      onClick={() => act('set_heat', { heat: 'Low' })}
+                      minWidth="75px"
+                      textAlign="center"
+                    >
+                      Low
+                    </Button>
+                  </Stack.Item>
+                </Stack.Item>
+              </Stack>
+              <Stack justify="center">
+                <Stack.Item>
+                  <Button onClick={() => act('start')} fontSize={2} mt={0.5}>
+                    Cook
+                  </Button>
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+          <Stack.Item grow>
+            <Section
+              title="Contents"
+              fill
+              scrollable={!!(content_icons && content_icons.length)}
+              buttons={
+                <Button onClick={() => act('eject_all')}>
+                  <Icon name="eject" />
+                </Button>
+              }
+            >
+              {content_icons?.length
+                ? content_icons.map((item, index) => (
+                    <Stack
+                      key={index}
+                      style={{ borderBottom: '0.5px #555 solid' }}
+                    >
+                      <Stack.Item
+                        grow
+                        style={{
+                          display: 'flex',
+                        }}
+                      >
+                        <Image
+                          height="32px"
+                          width="32px"
+                          src={`data:image/png;base64,${item}`}
+                          style={{ transform: 'translate(0, -2px)' }}
+                        />
+                        <Box mt={1}>{content_names[index]}</Box>
+                      </Stack.Item>
+                      <Stack.Item>
+                        <Button
+                          onClick={() =>
+                            act('eject', { ejected_item: index + 1 })
+                          }
+                          ml={3}
+                          style={{ transform: 'translate(0, 4px)' }}
                         >
-                          <Image
-                            height="32px"
-                            width="32px"
-                            src={`data:image/png;base64,${item}`}
-                            style={{ transform: 'translate(0, -2px)' }}
-                          />
-                          <Box mt={1}>{recipe_names[index]}</Box>
+                          <Icon name="eject" />
+                        </Button>
+                      </Stack.Item>
+                    </Stack>
+                  ))
+                : '(Empty)'}
+            </Section>
+          </Stack.Item>
+          <Stack.Item>
+            <Section>
+              <Stack>
+                <Stack.Item grow>
+                  <Section title="Potential Recipe">
+                    {output_icon ? (
+                      <Stack vertical>
+                        <Stack.Item>
+                          {recipe_icons.map((item, index) => (
+                            <Stack.Item
+                              key={index}
+                              style={{
+                                borderBottom: '0.5px #555 solid',
+                                display: 'flex',
+                              }}
+                            >
+                              <Image
+                                height="32px"
+                                width="32px"
+                                src={`data:image/png;base64,${item}`}
+                                style={{ transform: 'translate(0, -2px)' }}
+                              />
+                              <Box mt={1}>{recipe_names[index]}</Box>
+                            </Stack.Item>
+                          ))}
                         </Stack.Item>
-                      ))}
-                    </Stack.Item>
-                    <Stack.Item>
-                      <LabeledList>
-                        <LabeledList.Item label="Cook time">
-                          {cook_time}
-                        </LabeledList.Item>
-                      </LabeledList>
-                    </Stack.Item>
-                  </Stack>
-                ) : (
-                  'N/A'
-                )}
-              </Section>
-            </Stack.Item>
-            <Stack.Item>
-              <Divider vertical />
-            </Stack.Item>
-            <Stack.Item grow>
-              <Section title="Result">
-                {output_icon ? (
-                  <Stack vertical align="center" textAlign="center">
-                    <Stack.Item>
-                      <Image
-                        height="64px"
-                        width="64px"
-                        src={`data:image/png;base64,${output_icon}`}
-                      />
-                    </Stack.Item>
-                    <Stack.Item>{output_name}</Stack.Item>
-                  </Stack>
-                ) : (
-                  'N/A'
-                )}
-              </Section>
-            </Stack.Item>
-          </Stack>
-        </Section>
+                        <Stack.Item>
+                          <LabeledList>
+                            <LabeledList.Item label="Cook time">
+                              {cook_time}
+                            </LabeledList.Item>
+                          </LabeledList>
+                        </Stack.Item>
+                      </Stack>
+                    ) : (
+                      'N/A'
+                    )}
+                  </Section>
+                </Stack.Item>
+                <Stack.Item>
+                  <Divider vertical />
+                </Stack.Item>
+                <Stack.Item grow>
+                  <Section title="Result">
+                    {output_icon ? (
+                      <Stack vertical align="center" textAlign="center">
+                        <Stack.Item>
+                          <Image
+                            height="64px"
+                            width="64px"
+                            src={`data:image/png;base64,${output_icon}`}
+                          />
+                        </Stack.Item>
+                        <Stack.Item>{output_name}</Stack.Item>
+                      </Stack>
+                    ) : (
+                      'N/A'
+                    )}
+                  </Section>
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+        </Stack>
         {!!cooking && (
           <Modal fontSize={2} textAlign="center">
             <Section>Cooking! Please wait...</Section>
