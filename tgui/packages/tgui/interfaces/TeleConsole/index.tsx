94c53bc6031e16aa65bcc23f36f9c1814a216e41
iiOxygen <xDis4ster@gmail.com>
2025-07-02 14:06:23 +0300

    thought about it, didn't like it

--- tgui/packages/tgui/interfaces/TeleConsole/index.tsx
+++ tgui/packages/tgui/interfaces/TeleConsole/index.tsx
@@ -60,7 +60,7 @@ export const TeleConsole = () => {
 
   return (
     <Window theme="ntos" width={410} height={515}>
-      <Window.Content scrollable textAlign="center">
+      <Window.Content textAlign="center">
         <Stack vertical fill>
           <Stack.Item>
             <Tabs>
