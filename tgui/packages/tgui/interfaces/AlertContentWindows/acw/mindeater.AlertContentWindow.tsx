6d86816a5426e20f3be938dce91f30009571b723
FlameArrow57 <53062374+FlameArrow57@users.noreply.github.com>
2025-07-27 11:38:34 -0700

    improve antag popup

--- tgui/packages/tgui/interfaces/AlertContentWindows/acw/mindeater.AlertContentWindow.tsx
+++ tgui/packages/tgui/interfaces/AlertContentWindows/acw/mindeater.AlertContentWindow.tsx
@@ -25,29 +25,26 @@ const MindeaterContentWindow = () => {
         state upon death.
       </p>
       <p>
-        2. Your goal is to gain Intellect from crew members! Use your ability
-        Brain Drain to do so, which will award you with Intellect. Stealth and
-        your Disguise ability will help you.
+        2. You cloak while in darkness when in your true form. Being in light
+        for too long, or using most abilities, will reveal you. A visibility
+        indicator next to you shows if you are visible or not.
       </p>
       <p>
-        3. You cloak while in darkness. Being in light for too long, attacking,
-        or using most abilities will reveal you. A visibility indicator next to
-        you shows if you are visible or not.
+        3. To start an attack, disguise with your Disguise ability. Use Brain
+        Drain to start draining the brain of a person, which will award you with
+        Intellect. If they try to run, use Paralyze to pull them towards you.
+        When you reach 100 Intellect on them, use Pierce the Veil to send them
+        to your realm.
       </p>
       <p>
-        4. Your basic attack is a ranged attack that drains chemicals from those
-        hit, replacing them with toxins.
+        4. Brain Drain will cause brain damage, other types of damage, and drain
+        chemicals from targets which replaces them with toxin.
       </p>
       <p>
-        5. Start attacks by sneakily using Brain Drain on crew members. Stay in
-        range while they become aware of you, and finish off the attack by using
-        Pierce the Veil on the target.
-      </p>
-      <p>
-        6. You can speak over Intruder chat with the radio hotkey, or prefixing
+        5. You can speak over Intruder chat with the radio hotkey, or prefixing
         your message with <em>:int</em>.
       </p>
-      <p>7. Your goal is to just cause as much chaos as possible!</p>
+      <p>6. Your goal is to just cause chaos. Have fun!</p>
       <p>This is a work in progress antag and will not be found on the wiki.</p>
     </div>
   );
