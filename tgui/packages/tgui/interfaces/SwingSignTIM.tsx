a8c2e876ebc0366e71a70a60dc3a98b2af1adf28
ZeWaka <zewakagamer@gmail.com>
2025-08-07 20:52:38 -0700

    fix swingsigns

--- tgui/packages/tgui/interfaces/SwingSignTIM.tsx
+++ tgui/packages/tgui/interfaces/SwingSignTIM.tsx
@@ -36,12 +36,10 @@ export const SwingSignTIM = () => {
     isValid: allowEmpty || !!message,
     error: null,
   });
-  const onType = (event) => {
-    event.preventDefault();
-    const target = event.target;
-    target.value = trimText(target.value, rows, columns);
-    setInputIsValid(validateInput(target.value, max_length, rows));
-    setInput(target.value);
+  const onType = (value: string) => {
+    const trimmedValue = trimText(value, rows, columns);
+    setInputIsValid(validateInput(trimmedValue, max_length, rows));
+    setInput(trimmedValue);
   };
   // Dynamically changes the window height based on the message.
   const windowHeight = 130 + Math.ceil(message.length / 5) + 75;
@@ -70,7 +68,7 @@ export const SwingSignTIM = () => {
 
 interface InputAreaProps {
   input: string;
-  onType: (v: string) => void;
+  onType: (value: string) => void;
 }
 
 /** Gets the user input and invalidates if there's a constraint. */
