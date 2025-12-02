goonstation/goonstation

commit 0ab0dae9b7a1fe1faa0ff86250a827e5785aa591

Allow for slower generation of Find Planets to minimize gameplay impact in a round

code/WorkInProgress/salvager/salvager_magpie.dm
@@ -303,6 +303,7 @@ ABSTRACT_TYPE(/datum/commodity/magpie/buy/random_buy)
 	proc/reroll_commodity()
 		if(length(targets))
 			comtype = pick(targets)
+			targets -= comtype
 			var/obj/object_type = comtype
 			comname = initial(object_type.name)
 			var/value = targets[comtype]
@@ -310,7 +311,6 @@ ABSTRACT_TYPE(/datum/commodity/magpie/buy/random_buy)
 			baseprice = value
 			upperfluc = value * 0.10
 			lowerfluc = value * -0.05
-			targets -= comtype
 		else
 			comtype = null
 			hidden = TRUE

_std/map.dm
@@ -26,7 +26,7 @@
 #define MAPGEN_IGNORE_BUILDABLE (1 << 2)
 #define MAPGEN_ALLOW_VEHICLES (1 << 3)
 #define MAPGEN_FLOOR_ONLY	(1 << 4)
-
+#define MAPGEN_SLOW_GENERATION	(1 << 5)
 #define MAPGEN_TURF_ONLY ( MAPGEN_IGNORE_FLORA | MAPGEN_IGNORE_FAUNA )
 
 // map region allocator defines

code/WorkInProgress/artemis/GeneratePlanets.dm
@@ -214,6 +214,7 @@ DEFINE_PLANET(indigo, "Indigo")
 var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 
 /proc/GeneratePlanetChunk(width=null, height=null, prefabs_to_place=1, datum/map_generator/generator=/datum/map_generator/desert_generator, color=null, name=null, use_lrt=TRUE, seed_ore=TRUE, mapgen_flags=null)
+	var/startTime = world.timeofday
 	var/turf/T
 	if(istext(generator)) generator = text2path(generator)
 	if(ispath(generator)) generator = new generator()
@@ -283,8 +284,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 				T = region.turf_at(x, y)
 				if(T)
 					border_area.contents += T
-
-			generator.lag_check()
+			generator.lag_check(mapgen_flags)
 
 	//Lighten' Up the Place
 	var/image/ambient_light = new /image/ambient
@@ -328,7 +328,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 					generator.generate_terrain(space_turfs, reuse_seed=TRUE)
 					for(T in space_turfs)
 						T.AddOverlays(ambient_light, "ambient")
-						LAGCHECK(LAG_LOW)
+						generator.lag_check(mapgen_flags)
 
 					logTheThing(LOG_DEBUG, null, "Prefab placement #[n] [P.type][P.required?" (REQUIRED)":""] succeeded. [target] @ [log_loc(target)]")
 					n++
@@ -336,6 +336,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 				else
 					logTheThing(LOG_DEBUG, null, "Prefab placement #[n] [P.type] failed due to blocked area. [target] @ [log_loc(target)]")
 				count++
+				generator.lag_check(mapgen_flags)
 			if (count == maxTries)
 				logTheThing(LOG_DEBUG, null, "Prefab placement #[n] [P.type] failed due to maximum tries [maxTries][P.required?" WARNING: REQUIRED FAILED":""].")
 		else break
@@ -348,7 +349,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 			var/seed_density = clamp(length(mountains)/500, 2, 30)
 			for(var/j in 1 to seed_density)
 				Turfspawn_Asteroid_SeedOre(mountains, fullbright=FALSE)
-				LAGCHECK(LAG_LOW)
+				generator.lag_check(mapgen_flags)
 
 			for(var/i in 1 to seed_density/2)
 				if(length(mountains))
@@ -358,6 +359,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 						ast_list |= AST
 					Turfspawn_Asteroid_SeedOre(ast_list, veins=rand(1,3), rarity_mod=rand(0,40), fullbright=FALSE)
 					Turfspawn_Asteroid_SeedEvents(mountains)
+					generator.lag_check(mapgen_flags)
 
 	//Allow folks to like uh, get here?
 	if(use_lrt)
@@ -371,6 +373,7 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 			T = pick(turfs)
 			if(!checkTurfPassable(T))
 				maxTries--
+				generator.lag_check(mapgen_flags)
 				continue
 
 			new /obj/landmark/lrt/planet(T, name)
@@ -378,9 +381,10 @@ var/global/datum/planetManager/PLANET_LOCATIONS = new /datum/planetManager()
 			lrt_placed = TRUE
 			special_places.Add(name)
 
-	logTheThing(LOG_ADMIN, usr, "Planet region generated at [log_loc(region.bottom_left)] with [generator].")
-	logTheThing(LOG_DIARY, usr, "Planet region generated at [log_loc(region.bottom_left)] with [generator].", "admin")
-	message_admins("Planet region generated at [log_loc(region.bottom_left)] with [generator].")
+	var/gen_text = "Planet region generated at [log_loc(region.bottom_left)] with [generator] in [(world.timeofday - startTime)/10] seconds."
+	logTheThing(LOG_ADMIN, usr, gen_text)
+	logTheThing(LOG_DIARY, usr, gen_text, "admin")
+	message_admins(gen_text)
 
 	return turfs
 

code/modules/events/gimmick/generate_planet.dm
@@ -8,15 +8,16 @@ TYPEINFO(/datum/random_event/major/find_planet)
 		EVENT_INFO("generate_mobs", DATA_INPUT_BOOL, "Generate Mobs?"),
 		EVENT_INFO("seed_ore", DATA_INPUT_BOOL, "Generate Ore in Rocks/Mountains"),
 		EVENT_INFO("color", DATA_INPUT_COLOR, "Planet Color"),
+		EVENT_INFO("slow_generation", DATA_INPUT_BOOL, "Slow generation?"),
 		EVENT_INFO("delay_finalization", DATA_INPUT_BOOL, "Delay Finalization?")
 	)
 
 
 /datum/random_event/major/find_planet
 	name = "Find Planet"
-	required_elapsed_round_time = 15 MINUTES
+	required_elapsed_round_time = 28 MINUTES
 	customization_available = 1
-	disabled = TRUE
+	weight = 10
 
 	var/generate_mobs = TRUE
 	var/add_lrt = TRUE
@@ -28,7 +29,9 @@ TYPEINFO(/datum/random_event/major/find_planet)
 	var/generator = null
 	var/planet_name = null
 	var/admin_customized = FALSE
+	var/slow_generation = TRUE
 	var/color = null
+	var/lock = FALSE
 
 	admin_call(var/source)
 		if (..())
@@ -69,92 +72,100 @@ TYPEINFO(/datum/random_event/major/find_planet)
 	event_effect()
 		..()
 
-		var/blacklist_generators = list(/datum/map_generator/icemoon_generator,
-										/datum/map_generator/mars_generator,
-										/datum/map_generator/void_generator,
-										/datum/map_generator/asteroids,
-										/datum/map_generator/sea_caves,
-										/datum/map_generator/storehouse_generator,
-										/datum/map_generator/storehouse_generator/meaty,
-										/datum/map_generator/room_maze_generator,
-										/datum/map_generator/room_maze_generator/random,
-										/datum/map_generator/room_maze_generator/spatial)
+		if (src.lock)
+			message_admins("Previous Find Planet event hasn't finished yet, aborting.")
+			return
+		src.lock = TRUE
 
-		if(isnull(generator))
-			generator = pick(childrentypesof(/datum/map_generator)-blacklist_generators)
+		SPAWN(0)
+			var/blacklist_generators = list(/datum/map_generator/icemoon_generator,
+											/datum/map_generator/mars_generator,
+											/datum/map_generator/void_generator,
+											/datum/map_generator/asteroids,
+											/datum/map_generator/sea_caves,
+											/datum/map_generator/storehouse_generator,
+											/datum/map_generator/storehouse_generator/meaty,
+											/datum/map_generator/room_maze_generator,
+											/datum/map_generator/room_maze_generator/random,
+											/datum/map_generator/room_maze_generator/spatial)
 
-		if(!admin_customized)
-			if(!color && prob(10))
-				color = pick("#222", "#444", "#666", "#844", "#884", "#448", "#288")
-			prefabs = rand(2, 3)
+			if(isnull(generator))
+				generator = pick(childrentypesof(/datum/map_generator)-blacklist_generators)
 
+			if(!admin_customized)
+				if(!color && prob(10))
+					color = pick("#222", "#444", "#666", "#844", "#884", "#448", "#288")
+				prefabs = rand(2, 3)
 
-		if(!planet_name)
-			planet_name = ""
-			if (prob(50))
-				planet_name += pick_string("station_name.txt", "greek")
+			if(!planet_name)
+				planet_name = ""
+				if (prob(50))
+					planet_name += pick_string("station_name.txt", "greek")
+				else
+					planet_name += pick_string("station_name.txt", "militaryLetters")
+				planet_name += " "
+
+				if (prob(30))
+					planet_name += pick_string("station_name.txt", "romanNum")
+				else
+					planet_name += "[rand(2, 99)]"
+
+			var/flags = 0
+			if(!generate_mobs)
+				flags |= MAPGEN_IGNORE_FAUNA
+
+			if(slow_generation)
+				flags |= MAPGEN_SLOW_GENERATION
+
+			if(delay_finalization && add_lrt)
+
+				var/list/turf/turfs = GeneratePlanetChunk(src.width, src.height, prefabs_to_place=src.prefabs, generator=src.generator, color=src.color, name=planet_name, use_lrt=FALSE, seed_ore=src.seed_ore, mapgen_flags=flags)
+				if(!length(turfs))
+					message_admins("Planet Generation Failed!")
+
+				tgui_alert(usr, "Continue when you are complete...","Ready?!?",list("Continue"))
+
+				var/lrt_placed = FALSE
+				var/maxTries = 80
+				while(!lrt_placed)
+					if(!maxTries)
+						message_admins("Planet region failed to place LRT coordinates!!!")
+						break
+
+					var/turf/T = pick(turfs)
+					if(!checkTurfPassable(T))
+						maxTries--
+						continue
+
+					new /obj/landmark/lrt/planet(T, planet_name)
+					new /obj/decal/teleport_mark(T)
+					lrt_placed = TRUE
+					special_places.Add(planet_name)
 			else
-				planet_name += pick_string("station_name.txt", "militaryLetters")
-			planet_name += " "
+				GeneratePlanetChunk(src.width, src.height, prefabs_to_place=src.prefabs, generator=src.generator, color=src.color, name=planet_name, use_lrt=src.add_lrt, seed_ore=src.seed_ore, mapgen_flags=flags)
 
-			if (prob(30))
-				planet_name += pick_string("station_name.txt", "romanNum")
-			else
-				planet_name += "[rand(2, 99)]"
+			var/sound_to_play = 'sound/misc/announcement_1.ogg'
+			var/title = pick("Planetary Data Received","URGENT - Exploration Mission","Exploration Mission to [planet_name]")
+			var/list/reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevant equipment or technology.", \
+			"Our company has been granted permission to explore [planet_name], a nearby planet that has shown signs of potential habitability. The goals of the mission are to conduct a thorough survey of the planet's surface and search for signs of past or present life.", \
+			"Urgent request has been made to explore [planet_name], a nearby planet that has shown signs of potential habitability. Due to the pressing nature of this request, please depart immediately. The goals of the mission are to conduct a quick survey of the planet's surface and search for any signs of life.", \
+			"We have received approval to explore [planet_name], a nearby planet of great interest to us.  This is a highly secretive and sensitive mission, and I trust that all employees will maintain strict confidentiality.")
 
-		var/flags = 0
-		if(!generate_mobs)
-			flags |= MAPGEN_IGNORE_FAUNA
+			if(generator == /datum/map_generator/void_generator)
+				title = pick("Anamolous Data Received", "URGENT - Exploration Mission")
+				reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevant equipment or technology.")
+			else if(generator == /datum/map_generator/asteroids)
+				title = pick("Exploration Mission to [planet_name]", "URGENT - Exploration Mission")
+				reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevant equipment or technology.", \
+			"Our company has been granted permission to explore [planet_name], a nearby cluster of asteroids. The goals of the mission are to conduct a thorough survey of the region and search for signs of past or present life.", \
+			"Urgent request has been made to explore [planet_name], a nearby asteroid that has shown signs of valuable minerals. Due to the pressing nature of this request, please depart immediately. The goals of the mission are to conduct a quick survey of the region.", \
+			"We have received approval to explore [planet_name], this group of asteroids if of great interest to us.  This is a highly secretive and sensitive mission, and I trust that all employees will maintain strict confidentiality.")
 
-		if(delay_finalization && add_lrt)
+			var/command_report = pick(reports)
+			command_report += "\n\nTarget data sent to the teleport computer."
+			command_announcement(replacetext(command_report, "\n", "<br>"), title, sound_to_play, do_sanitize=0);
 
-			var/list/turf/turfs = GeneratePlanetChunk(src.width, src.height, prefabs_to_place=src.prefabs, generator=src.generator, color=src.color, name=planet_name, use_lrt=FALSE, seed_ore=src.seed_ore, mapgen_flags=flags)
-			if(!length(turfs))
-				message_admins("Planet Generation Failed!")
-
-			tgui_alert(usr, "Continue when you are complete...","Ready?!?",list("Continue"))
-
-			var/lrt_placed = FALSE
-			var/maxTries = 80
-			while(!lrt_placed)
-				if(!maxTries)
-					message_admins("Planet region failed to place LRT coordinates!!!")
-					break
-
-				var/turf/T = pick(turfs)
-				if(!checkTurfPassable(T))
-					maxTries--
-					continue
-
-				new /obj/landmark/lrt/planet(T, planet_name)
-				new /obj/decal/teleport_mark(T)
-				lrt_placed = TRUE
-				special_places.Add(planet_name)
-		else
-			GeneratePlanetChunk(src.width, src.height, prefabs_to_place=src.prefabs, generator=src.generator, color=src.color, name=planet_name, use_lrt=src.add_lrt, seed_ore=src.seed_ore, mapgen_flags=flags)
-
-		var/sound_to_play = 'sound/misc/announcement_1.ogg'
-		var/title = pick("Planetary Data Received","URGENT - Exploration Mission","Exploration Mission to [planet_name]")
-		var/list/reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevent equipment or technology.", \
-		"Our company has been granted permission to explore [planet_name], a nearby planet that has shown signs of potential habitability. The goals of the mission are to conduct a thorough survey of the planet's surface and search for signs of past or present life.", \
-		"Urgent request has been made to explore [planet_name], a nearby planet that has shown signs of potential habitability. Due to the pressing nature of this request, please depart immediately. The goals of the mission are to conduct a quick survey of the planet's surface and search for any signs of life.", \
-		"We have received approval to explore [planet_name], a nearby planet of great interest to us.  This is a highly secretive and sensitive mission, and I trust that all employees will maintain strict confidentiality.")
-
-		if(generator == /datum/map_generator/void_generator)
-			title = pick("Anamolous Data Received", "URGENT - Exploration Mission")
-			reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevent equipment or technology.")
-		else if(generator == /datum/map_generator/asteroids)
-			title = pick("Exploration Mission to [planet_name]", "URGENT - Exploration Mission")
-			reports = list("Unusual readings detected on [planet_name].  Investigate and bring back any relevent equipment or technology.", \
-		"Our company has been granted permission to explore [planet_name], a nearby cluster of asteroids. The goals of the mission are to conduct a thorough survey of the region and search for signs of past or present life.", \
-		"Urgent request has been made to explore [planet_name], a nearby asteroid that has shown signs of valuable minerals. Due to the pressing nature of this request, please depart immediately. The goals of the mission are to conduct a quick survey of the region.", \
-		"We have received approval to explore [planet_name], this group of asteroids if of great interest to us.  This is a highly secretive and sensitive mission, and I trust that all employees will maintain strict confidentiality.")
-
-		var/command_report = pick(reports)
-		command_report += "\n\nTarget data sent to the teleport computer."
-		command_announcement(replacetext(command_report, "\n", "<br>"), title, sound_to_play, do_sanitize=0);
-
-		post_event()
+			post_event()
 
 	proc/post_event()
 		src.generate_mobs = initial(src.generate_mobs)
@@ -168,4 +179,5 @@ TYPEINFO(/datum/random_event/major/find_planet)
 		src.color = initial(src.color)
 		src.generator = initial(src.generator)
 		src.admin_customized = initial(src.admin_customized)
+		src.lock = FALSE
 		return

code/modules/worldgen/mapgen/CaveGenerator.dm
@@ -195,7 +195,7 @@
 		selected_biome = biomes[selected_biome]
 		selected_biome.generate_turf(gen_turf, flags)
 
-		src.lag_check()
+		src.lag_check(flags)
 
 
 /turf/simulated/wall/auto/asteroid/mountain/cave

code/modules/worldgen/mapgen/DesertGenerator.dm
@@ -81,7 +81,7 @@
 
 		gen_turf.temperature = 330 // 56.9C
 
-		src.lag_check()
+		src.lag_check(flags)
 
 
 ///for the mapgen mountains, temp until we get something better

code/modules/worldgen/mapgen/ForestGenerator.dm
@@ -80,7 +80,7 @@
 		selected_biome = biomes[selected_biome]
 		selected_biome.generate_turf(gen_turf, flags)
 
-		src.lag_check()
+		src.lag_check(flags)
 
 /datum/biome/forest/generate_turf(turf/gen_turf, flags)
 	. = ..()

commit e4117ad63727bf90a5e872bf23c7aa16bc20a7be

Fixed salvager bounties not having a value caused by 0ab0dae9b7a1

code/WorkInProgress/salvager/salvager_magpie.dm
@@ -275,12 +275,12 @@ ABSTRACT_TYPE(/datum/commodity/magpie/buy/random_buy)
 	var/list/targets = list()
 
 	proc/reroll_commodity()
-		if(length(targets))
-			comtype = pick(targets)
-			targets -= comtype
-			var/obj/object_type = comtype
-			comname = initial(object_type.name)
-			var/value = targets[comtype]
+		if(length(src.targets))
+			src.comtype = pick(src.targets)
+			var/value = src.targets[src.comtype]
+			src.targets -= src.comtype
+			var/obj/object_type = src.comtype
+			src.comname = initial(object_type.name)
 			price = value
 			baseprice = value
 			upperfluc = value * 0.10