1e678795c5570f020a3e2010f07991b12d4a0513
Mordent <mordent.goonstation@gmail.com>
2026-08-13 14:51:32 -0400

    Split DiskDrive component's generic layer out into own component, use within DiskDrive

---
+++ tgui/packages/tgui/interfaces/common/drives/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * @copyright 2026
+ * @author Mordent (https://github.com/mordent-goonstation)
+ * @license MIT
+ */
+
+export { DiskDrive } from './DiskDrive';
+export { Drive } from './Drive';
+export type { DriveBaseProps, DriveSlotProps } from './type';
