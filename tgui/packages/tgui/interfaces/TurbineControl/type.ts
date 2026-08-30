eca306d00eab3d08870c9915caa3137e8712a7bd
Sov <33204415+Sovexe@users.noreply.github.com>
2026-08-29 22:44:21 -0400

    Gas turbine UI (#27571)

---
+++ tgui/packages/tgui/interfaces/TurbineControl/type.ts
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * @copyright 2026
+ * @author Sovexe  (https://github.com/Sovexe)
+ * @license MIT
+ */
+
+import type { BooleanLike } from 'tgui-core/react';
+
+export interface TurbineHistorySample {
+  rpm: number;
+  load: number;
+  power: number;
+  temperature: number;
+  pressure: number;
+  outletTemperature: number;
+  outletPressure: number;
+}
+
+export interface TurbineBladeData {
+  installed: BooleanLike;
+  healthPercent: number;
+}
+
+export interface TurbineStatorData {
+  installed: BooleanLike;
+}
+
+export interface TurbineControlData {
+  connected: BooleanLike;
+  rpm: number;
+  load: number;
+  power: number;
+  volume: number;
+  volumeMax: number;
+  history: TurbineHistorySample[];
+  optimalRPM: number;
+  overspeedRPM: number;
+  statorLoadMin: number;
+  statorLoadMax: number;
+  flowRateMin: number;
+  minimumTemperature: number;
+  overtempWarning: number;
+  overtempLimit: number;
+  inletTemperature: number;
+  inletPressure: number;
+  outletTemperature: number;
+  outletPressure: number;
+  overspeed: BooleanLike;
+  overtemp: BooleanLike;
+  undertemp: BooleanLike;
+  stalling: BooleanLike;
+  ruined: BooleanLike;
+  blade: TurbineBladeData | null;
+  stator: TurbineStatorData | null;
+}
